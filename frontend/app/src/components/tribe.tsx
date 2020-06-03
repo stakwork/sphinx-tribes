581074797d3b0e88ad4cf01488363fdb30483366
Evan Feenstra <evan@evanfeenstra.com>
2020-06-03 10:32:49 -0700

    real fields in new design

--- frontend/app/src/components/tribe.tsx
+++ frontend/app/src/components/tribe.tsx
@@ -1,113 +1,115 @@
 import React, {useRef, useState} from 'react'
 import { QRCode } from 'react-qr-svg';
 import styled from 'styled-components'
-import {
-  EuiCheckableCard,
-  EuiSpacer,
-  EuiHighlight,
-} from '@elastic/eui';
+import {EuiCheckableCard} from '@elastic/eui';
 import Tag from './tag'
+import moment from 'moment'
 
 function makeQR(uuid:string){
   return `sphinx.chat://?action=tribe&uuid=${uuid}&host=${window.location.hostname}`
 }
-function copyString(e:any, textareaRef:any) {
-  e.stopPropagation();
-  textareaRef.current.select();
-  document.execCommand('copy');
-  return alert('String copied!' )
-}
 
-export default function Tribe({uuid,name,img,tags,description,selected,select}:any){
+
+export default function Tribe({uuid,name,img,tags,description,selected,select,created,owner_alias,price_to_join,price_per_message}:any){
   const showTags = tags&&tags.length&&tags.length>0?true:false
   const textareaRef = useRef(null);
-  return <>
-    <EuiCheckableCard className="col-md-6 col-lg-6 ml-2 mb-2"
-      id={uuid}
-      label={name}
-      name={name}
-      value={uuid}
-      checked={selected}
-      onChange={() => select(uuid)}>
-      <Content onClick={() => select(selected?'':uuid)} style={{
-        height:selected?'auto':100
-      }} selected={selected}>
-        <Left >
-          <Row className="item-cont">
-            <div className="placeholder-img-tribe"></div>
-            <Img src={img} />
-            <Left style={{padding:'0 0 0 20px', maxWidth:'calc(100% - 100px)'}}>
-              <Row style={selected?{flexDirection:'column',alignItems:'flex-start'}:{}}>
-                <Title className="tribe-title">
-                  {name}
-                </Title>
-              </Row>
-              <Description oneLine={selected?false:true} style={{minHeight:20}}>
-                {description}
-              </Description>
+  const qrString = makeQR(uuid)
+  const [copied,setCopied] = useState(false)
 
-              {showTags && <Tokens className="tags-wrapper">
-                {tags.map((t:string)=> <Tag type={t} key={t} />)}
-                {tags.length > 3 && <span className="more-tags-available">more...</span>}
-              </Tokens>}
-            </Left>
-          </Row>
-          <div className="expand-part" style={selected ? { opacity: 1} : { opacity: 0}}>
-            <div className="section-separator"></div>
-            <div className="row info-section">
-              <div className="col-4 col-sm-4 col-md-4 col-lg-4">
-                <div className="uppercase">Admin:</div>
-                <div className="lighter-color">AdminUsername</div>
-              </div>
-              <div className="col-4 col-sm-4 col-md-4 col-lg-4 text-center">
-                <div className="uppercase">Members:</div>
-                <div className="lighter-color">42</div>
-              </div>
-              <div className="col-4 col-sm-4 col-md-4 col-lg-4 text-right">
-                <div className="uppercase">Created on:</div>
-                <div className="lighter-color">May 22</div>
-              </div>
+  function copyString(e:any, textareaRef:any) {
+    e.stopPropagation();
+    textareaRef.current.select();
+    document.execCommand('copy');
+    setCopied(true)
+    setTimeout(()=>setCopied(false),2700)
+  }
+
+  return <EuiCheckableCard className="col-md-6 col-lg-6 ml-2 mb-2"
+    id={uuid}
+    label={name}
+    name={name}
+    value={uuid}
+    checked={selected}
+    onChange={() => select(uuid)}>
+    <Content onClick={() => select(selected?'':uuid)} style={{
+      height:selected?'auto':100
+    }} selected={selected}>
+      <Left>
+        <Row className="item-cont">
+          <div className="placeholder-img-tribe"></div>
+          <Img src={img} />
+          <Left style={{padding:'0 0 0 20px', maxWidth:'calc(100% - 100px)'}}>
+            <Row style={selected?{flexDirection:'column',alignItems:'flex-start'}:{}}>
+              <Title className="tribe-title">
+                {name}
+              </Title>
+            </Row>
+            <Description oneLine={selected?false:true} style={{minHeight:20}}>
+              {description}
+            </Description>
+
+            {showTags && <Tokens className="tags-wrapper">
+              {tags.map((t:string)=> <Tag type={t} key={t} />)}
+              {tags.length > 3 && <span className="more-tags-available">more...</span>}
+            </Tokens>}
+          </Left>
+        </Row>
+        <div className="expand-part" style={selected ? { opacity: 1} : { opacity: 0}}>
+          <div className="section-separator"></div>
+          <div className="row info-section">
+            <div className="col-4 col-sm-4 col-md-4 col-lg-4">
+              <div className="uppercase">Admin:</div>
+              <div className="lighter-color">{owner_alias}</div>
             </div>
-            <div className="section-separator"></div>
-
-            <div className="row">
-              <div className="col-4 col-sm-4 col-md-4 col-lg-4 qr-left">
-                <div className="text-right"><img style={{width: 100}} src="static/scan_notification.svg" alt="" /></div>
-                <div className="text-right info">
-                  <div>Price to join</div>
-                  <div className="lighter-color">2500</div>
-                </div>
-                <div className="section-separator"></div>
-                <div className="text-right info disabled">
-                  <div>Price per msg</div>
-                  <div className="lighter-color">0</div>
-                </div>
-                <div className="section-separator"></div>
-              </div>
-              <div className="col-8 col-sm-8 col-md-8 col-lg-8">
-                {selected && <QRWrap className="qr-wrap float-r">
-                  <QRCode
-                    bgColor={selected?'#FFFFFF':'#666'}
-                    fgColor="#000000"
-                    level="Q"
-                    style={{width:209}}
-                    value={makeQR(uuid)}
-                  />
-                </QRWrap>}
-                <div className="below-qr">
-                  <textarea className="qr-string" ref={textareaRef} >14unzosz69hGfghj614unzosz69hGfghj614unzosz69hGfghj614unzosz69hGfghj614unzosz69hGfghj6</textarea>
-                  <button className="copy-btn" onClick={(e) => copyString(e, textareaRef)}>Copy</button>
-                </div>
-              </div>
+            <div className="col-4 col-sm-4 col-md-4 col-lg-4 text-center">
+              {/* <div className="uppercase">Members:</div>
+              <div className="lighter-color">42</div> */}
             </div>
-
-            <div className="colapse-button"><img src="static/keyboard_arrow_up-black-18dp.svg" alt="" /></div>
+            <div className="col-4 col-sm-4 col-md-4 col-lg-4 text-right">
+              <div className="uppercase">Created on:</div>
+              <div className="lighter-color">{moment(created).format('MMM D')}</div>
+            </div>
           </div>
-        </Left>
-      </Content>
-    </EuiCheckableCard>
+          <div className="section-separator"></div>
 
-  </>
+          <div className="row">
+            <div className="col-4 col-sm-4 col-md-4 col-lg-4 qr-left">
+              <div className="text-right"><img style={{width: 100}} src="static/scan_notification.svg" alt="" /></div>
+              <div className="text-right info">
+                <div>Price to join</div>
+                <div className="lighter-color">{price_to_join||0}</div>
+              </div>
+              <div className="section-separator"></div>
+              <div className="text-right info">
+                <div>Price per msg</div>
+                <div className="lighter-color">{price_per_message||0}</div>
+              </div>
+              <div className="section-separator"></div>
+            </div>
+            <div className="col-8 col-sm-8 col-md-8 col-lg-8">
+              {selected && <QRWrap className="qr-wrap float-r">
+                <QRCode
+                  bgColor={selected?'#FFFFFF':'#666'}
+                  fgColor="#000000"
+                  level="Q"
+                  style={{width:209}}
+                  value={qrString}
+                />
+              </QRWrap>}
+              <div className="below-qr">
+                <textarea className="qr-string" ref={textareaRef} defaultValue={qrString} />
+                <button className="copy-btn" onClick={(e) => copyString(e, textareaRef)}>
+                  {copied?'Copied!':'Copy'}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="colapse-button"><img src="static/keyboard_arrow_up-black-18dp.svg" alt="" /></div>
+        </div>
+      </Left>
+    </Content>
+  </EuiCheckableCard>
 }
 interface ContentProps {
   selected: boolean;
