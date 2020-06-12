040fe95e7acc7f9623723d9f3e6224aa459ad74a
Evan Feenstra <evan@evanfeenstra.com>
2020-06-12 14:46:54 -0700

    add escrow_amount and escrow_millis, fix some layout issues on mobile ios

--- db.go
+++ db.go
@@ -42,6 +42,7 @@ func initDB() {
 var updatables = []string{
 	"name", "description", "tags", "img",
 	"owner_alias", "price_to_join", "price_per_message",
+	"escrow_amount", "escrow_millis",
 }
 
 // check that update owner_pub_key does in fact throw error
