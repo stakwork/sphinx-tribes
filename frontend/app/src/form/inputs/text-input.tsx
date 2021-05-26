ddf8797b5ac015ab295d5d5deb6f300a44aa9fee
Evanfeenstra <evanfeenstra@gmail.com>
2021-05-26 10:10:31 -0700

    fixes

--- frontend/app/src/form/inputs/text-input.tsx
+++ frontend/app/src/form/inputs/text-input.tsx
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 import {EuiFormRow, EuiFieldText} from '@elastic/eui'
 import type {Props} from './propsType'
 
-export default function TextInput({label, value, handleChange, handleBlur, handleFocus}:Props) {
+export default function TextInput({label, value, handleChange, handleBlur, handleFocus, readOnly}:Props) {
   return <EuiFormRow label={label}>
     <Text name="first" value={value} 
+      readOnly={readOnly||false}
       onChange={e => handleChange(e.target.value)}
       onBlur={handleBlur}
       onFocus={handleFocus}
