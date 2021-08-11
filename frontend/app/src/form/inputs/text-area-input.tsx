26655e9cc668d3447eff9553b84594e80b8127f1
David Plell <plelldavid@gmail.com>
2021-08-11 15:59:46 -0700

    form validation

--- frontend/app/src/form/inputs/text-area-input.tsx
+++ frontend/app/src/form/inputs/text-area-input.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
-import { EuiFormRow, EuiTextArea } from '@elastic/eui'
+import { EuiFormRow, EuiTextArea, EuiIcon } from '@elastic/eui'
 import type { Props } from './propsType'
 
-export default function TextAreaInput({ label, value, handleChange, handleBlur, handleFocus, readOnly, prepend, extraHTML }: Props) {
+export default function TextAreaInput({ error, label, value, handleChange, handleBlur, handleFocus, readOnly, prepend, extraHTML }: Props) {
     // console.log("TEXTAREA", label, extraHTML)
+
+    let labeltext = label
+    if (error) labeltext = labeltext + ` (${error})`
+
     return <>
-        <EuiFormRow label={label}>
-            <Text name="first" value={value || ''}
-                readOnly={readOnly || false}
-                onChange={e => handleChange(e.target.value)}
-                onBlur={handleBlur}
-                onFocus={handleFocus}
-            // prepend={prepend}
-            />
+        <EuiFormRow label={labeltext}>
+            <R>
+                <Text name="first" value={value || ''}
+                    readOnly={readOnly || false}
+                    onChange={e => handleChange(e.target.value)}
+                    onBlur={handleBlur}
+                    onFocus={handleFocus}
+                // prepend={prepend}
+                />
+                {error && <E>
+                    <EuiIcon type="alert" size='m' style={{ width: 20, height: 20 }} />
+                </E>}
+            </R>
         </EuiFormRow>
         <ExtraText
             style={{ display: value && extraHTML ? 'block' : 'none' }}
@@ -31,4 +40,19 @@ const ExtraText = styled.div`
   max-width:calc(100% - 20px);
   word-break: break-all;
   font-size:14px;
+`
+
+const E = styled.div`
+  position:absolute;
+  right:10px;
+  top:10px;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  color:#45b9f6;
+  pointer-events:none;
+  user-select:none;
+`
+const R = styled.div`
+  position:relative
 `
