73e0112f872df663232a346e34125827256b1857
David Plell <plelldavid@gmail.com>
2021-09-01 11:27:51 -0700

    deploy route change

--- routes.go
+++ routes.go
@@ -35,12 +35,14 @@ func NewRouter() *http.Server {
 		r.Get("/t/manifest.json", frontend.ManifestRoute)
 		r.Get("/t/favicon.ico", frontend.FaviconRoute)
 		r.Get("/t/{unique_name}", frontend.IndexRoute)
+		r.Get("/t", frontend.IndexRoute)
 	})
 	r.Group(func(r chi.Router) {
 		r.Get("/p/static/*", frontend.StaticRoute)
 		r.Get("/p/manifest.json", frontend.ManifestRoute)
 		r.Get("/p/favicon.ico", frontend.FaviconRoute)
 		r.Get("/p/{unique_name}", frontend.IndexRoute)
+		r.Get("/p", frontend.IndexRoute)
 	})
 
 	r.Group(func(r chi.Router) {
@@ -70,6 +72,14 @@ func NewRouter() *http.Server {
 		r.Post("/verify/{challenge}", verify)
 		r.Put("/bot", createOrEditBot)
 		r.Delete("/person/{id}", deletePerson)
+	})
+
+	// fallback routes redirect to
+	r.Group(func(r chi.Router) {
+		r.Get("/*", frontend.IndexRoute)
+		r.Get("/*/static/*", frontend.StaticRoute)
+		r.Get("/*/manifest.json", frontend.ManifestRoute)
+		r.Get("/*/favicon.ico", frontend.FaviconRoute)
 	})
 
 	PORT := os.Getenv("PORT")
