98d1b3f47e6d1d66893c21ce0d98d3504c0c5c42
David Plell <plelldavid@gmail.com>
2021-09-15 16:26:47 -0700

    mobile tabs

--- frontend/app/src/sphinxUI/search-text-input.tsx
+++ frontend/app/src/sphinxUI/search-text-input.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import MaterialIcon from '@material/react-material-icon'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useStores } from '../store'
 
 export default function SearchTextInput(props: any) {
-    return <Text
+    const { ui } = useStores()
+    const [expand, setExpand] = useState(ui.searchText ? true : false)
+
+    const collapseStyles = (props.small && !expand) ? {
+        width: 40, maxWidth: 40,
+    } : {}
+
+    return <div style={{ position: 'relative' }}><Text
         {...props}
+        onFocus={() => setExpand(true)}
+        onBlur={() => {
+            if (!ui.searchText) setExpand(false)
+        }}
         onChange={e => props.onChange(e.target.value)}
         placeholder={'Search'}
+        style={{ ...props.style, ...collapseStyles }}
     />
+        {(props.small && !expand) && <MaterialIcon icon='search' style={{
+            position: 'absolute', color: '#B0B7BC',
+            top: 9, left: 9, fontSize: 22, userSelect: 'none', pointerEvents: 'none'
+        }} />}
+    </div>
 }
 
 const Text = styled.input`
@@ -16,12 +35,12 @@ box-sizing: border-box;
 border-radius: 21px;
 padding-left:20px;
 padding-right:10px;
-width:100%;
+// width:100%;
 font-style: normal;
 font-weight: normal;
 font-size: 12px;
 line-height: 14px;
 height:35px;
+transition:all 0.4s;
 `
-
 
