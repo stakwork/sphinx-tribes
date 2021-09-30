75d4ca79dd5dd308ce5769dea06204af1d08375a
EvanFeenstra1 <evanfeenstra@gmail.com>
2021-09-30 15:45:16 -0700

    getGithubIssue route

--- routes.go
+++ routes.go
@@ -61,6 +61,7 @@ func NewRouter() *http.Server {
 		r.Get("/ask", ask)
 		r.Get("/poll/{challenge}", poll)
 		r.Get("/person/{pubkey}", getPersonByPubkey)
+		r.Get("/github_issue/{owner}/{repo}/{issue}", getGithubIssue)
 	})
 
 	r.Group(func(r chi.Router) {
