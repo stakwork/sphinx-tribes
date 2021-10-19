5e6c9852e0b503683025115e530145a20a03b759
David Plell <plelldavid@gmail.com>
2021-10-19 10:42:13 -0700

    merge

--- people.go
+++ people.go
@@ -239,6 +239,9 @@ func createOrEditPerson(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Println("people id")
+	fmt.Println(p.ID)
+
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(p)
 }
