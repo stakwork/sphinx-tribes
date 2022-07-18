a02549268325e5066bd9dae6d1b9e4656a6e4e22
kevkevin <oapallikunnel@gmail.com>
2022-07-18 16:19:43 -0600

    fixed line yml linebreaks for tickets (#243)
    * fixed line yml linebreaks for tickets
    
    * removed unwanted changes in diff
    
    * fixed text overflow issues on about page
    
    Co-authored-by: elraphty <elraphtyofficial@gmail.com>

---
+++ frontend/app/src/people/utils/renderMarkdown.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+export function renderMarkdown(markdown) {
+
+        return <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]}
+                components={{
+                        code({ node, inline, className, children, ...props }) {
+                                return (
+                                        <code className={className} {...props}>
+                                                {children}
+                                        </code>
+                                )
+                        },
+                        img({ className, ...props }) {
+                                return (
+                                        <img className={className}
+                                                style={{ width: '100%' }}
+                                                {...props} />
+                                )
+                        }
+                }} />
+}
