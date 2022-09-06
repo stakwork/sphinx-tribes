4633575094684c74d2fa0d4c3d3049e0cf76bbef
Evan Feenstra <evanfeenstra@gmail.com>
2022-09-06 10:12:09 -0700

    fix styled component typescipt issue

--- frontend/app/src/sphinxUI/select.tsx
+++ frontend/app/src/sphinxUI/select.tsx
@@ -1,40 +1,41 @@
-import React, { Fragment } from 'react'
-import styled from 'styled-components'
-import { EuiSuperSelect, EuiText } from '@elastic/eui'
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import { EuiSuperSelect, EuiText } from "@elastic/eui";
 
 export default function Select(props: any) {
-    const { options, onChange, value, style, selectStyle } = props
+  const { options, onChange, value, style, selectStyle } = props;
 
-    const opts = options.map((o) => {
-        return {
-            value: o.value,
-            inputDisplay: o.label,
-            dropdownDisplay: (
-                <Fragment>
-                    <strong>{o.label}</strong>
-                    <EuiText size="s" color="subdued">
-                        <p className="euiTextColor--subdued">
-                            {o.description}
-                        </p>
-                    </EuiText>
-                </Fragment>
-            ),
-        }
-    }) || []
+  const opts =
+    options.map((o) => {
+      return {
+        value: o.value,
+        inputDisplay: o.label,
+        dropdownDisplay: (
+          <Fragment>
+            <strong>{o.label}</strong>
+            <EuiText size="s" color="subdued">
+              <p className="euiTextColor--subdued">{o.description}</p>
+            </EuiText>
+          </Fragment>
+        ),
+      };
+    }) || [];
 
-    return <div style={{ position: 'relative', ...style }}>
-        <S
-            style={selectStyle}
-            options={opts}
-            valueOfSelected={value}
-            onChange={(value) => onChange(value)}
-            itemLayoutAlign="top"
-            hasDividers
-        />
+  return (
+    <div style={{ position: "relative", ...style }}>
+      <S
+        style={selectStyle}
+        options={opts}
+        valueOfSelected={value}
+        onChange={(value) => onChange(value)}
+        itemLayoutAlign="top"
+        hasDividers
+      />
     </div>
+  );
 }
 
-const S = styled(EuiSuperSelect)`
+const S = styled(EuiSuperSelect as any)`
 background:#ffffff00;
 border: 1px solid #E0E0E0;
 color:#000;
@@ -53,5 +54,4 @@ button {
     background-color:#ffffff !important;
 }
 }
-`
-
+`;
