07ff0fe12a204eddb8bfd06f68d36dface0e250a
kevkevin <oapallikunnel@gmail.com>
2022-09-21 15:17:31 -0500

    Fix add ticket button (#262)
    * add modal on People front page.
    
    * "Frontend work on tickets to add new ticket button, add a session length to tickets and an alert button on user profile"
    
    * fixed the mobile view
    
    Co-authored-by: Amit Paliwal <amit.paliwal@remotestate.com>

--- frontend/app/src/people/person.tsx
+++ frontend/app/src/people/person.tsx
@@ -65,7 +65,8 @@ export default function Person(props: any) {
   if (lastSeen === 'a month') lastSeen = '1 month'
 
   const defaultPic = '/static/person_placeholder.png'
-  const mediumPic = img 
+  const mediumPic = img && img + '?medium=true'
+
   const addedStyles = hideActions ? { width: 56, height: 56 } : {}
 
 
