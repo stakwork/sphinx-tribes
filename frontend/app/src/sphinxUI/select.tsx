804c8d3212b085d2507fbab088eb329c583dcc2b
Amit Paliwal <amit.paliwal@remotestate.com>
2022-09-23 17:47:31 +0530

    1. added session length on both, explore page and modal of the tickets. 2. fix super select css.

--- frontend/app/src/sphinxUI/select.tsx
+++ frontend/app/src/sphinxUI/select.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from "react";
-import styled from "styled-components";
-import { EuiSuperSelect, EuiText } from "@elastic/eui";
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import { EuiSuperSelect, EuiText } from '@elastic/eui';
 
 export default function Select(props: any) {
   const { options, onChange, value, style, selectStyle } = props;
@@ -11,47 +11,67 @@ export default function Select(props: any) {
         value: o.value,
         inputDisplay: o.label,
         dropdownDisplay: (
-          <Fragment>
-            <strong>{o.label}</strong>
-            <EuiText size="s" color="subdued">
-              <p className="euiTextColor--subdued">{o.description}</p>
-            </EuiText>
-          </Fragment>
+          <>
+            <p
+              style={{
+                color: '#000',
+                fontSize: '14px',
+                padding: '0px',
+                margin: 0,
+              }}
+            >
+              {o.label}
+            </p>
+            {o.description && (
+              <EuiText
+                size="s"
+                color="subdued"
+                style={{
+                  padding: 0,
+                  margin: 0,
+                  fontSize: '12px',
+                }}
+              >
+                <p className="euiTextColor--subdued">{o.description}</p>
+              </EuiText>
+            )}
+          </>
         ),
       };
     }) || [];
 
   return (
-    <div style={{ position: "relative", ...style }}>
+    <div style={{ position: 'relative', ...style }}>
       <S
-        style={selectStyle}
+        style={{
+          ...selectStyle,
+        }}
         options={opts}
         valueOfSelected={value}
         onChange={(value) => onChange(value)}
         itemLayoutAlign="top"
-        hasDividers
       />
     </div>
   );
 }
 
+//euiContextMenuItem euiSuperSelect__item euiSuperSelect__item--hasDividers
+
 const S = styled(EuiSuperSelect as any)`
-background:#ffffff00;
-border: 1px solid #E0E0E0;
-color:#000;
-box-sizing: border-box;
-box-shadow:none;
+  background: #ffffff00;
+  border: 1px solid #e0e0e0;
+  color: #000;
+  box-sizing: border-box;
+  box-shadow: none;
 
-user-select:none;
+  user-select: none;
 
-.euiSuperSelectControl.euiSuperSelect--isOpen__button{
-    background:#ffffff !important;
-    background-color:#ffffff !important;
-}
-
-button {
-    background:#ffffff !important;
-    background-color:#ffffff !important;
-}
-}
+  .euiSuperSelectControl.euiSuperSelect--isOpen__button {
+    background: #ffffff !important;
+    background-color: #ffffff !important;
+  }
+  button {
+    background: #ffffff !important;
+    background-color: #ffffff !important;
+  }
 `;
