c95aabeb522fe01c1e59323c5679125640e7c7ba
maradondt <maradondt@gmail.com>
2023-03-04 19:31:58 +0100

    Cancel the changes to host.ts

--- frontend/app/src/host.ts
+++ frontend/app/src/host.ts
@@ -2,10 +2,7 @@ const internalDockerHosts = ['localhost:13007', 'localhost:13000'];
 const externalDockerHosts = ['localhost:23007', 'localhost:23000'];
 
 export function getHost(): string {
-  const host = window.location.host.includes('localhost')
-    ? 'people.sphinx.chat'
-    : window.location.host;
-  // const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
+  const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
   return host;
 }
 
