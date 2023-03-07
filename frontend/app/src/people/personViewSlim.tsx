b0fb7480f303f72cf4404971a4e1de5c14632161
elraphty <elraphtyofficial@gmail.com>
2023-03-07 14:56:39 +0100

    added edit button for mobile screens

--- frontend/app/src/people/personViewSlim.tsx
+++ frontend/app/src/people/personViewSlim.tsx
@@ -476,22 +476,41 @@ export default function PersonView(props: any) {
           >
             <IconButton onClick={goBack} icon="arrow_back" />
             {canEdit ? (
-              <Button
-                text="Sign out"
-                onClick={logout}
-                height={42}
-                style={{
-                  fontSize: 13,
-                  color: '#3c3f41',
-                  border: 'none',
-                  margin: 0,
-                  padding: 0
-                }}
-                iconStyle={{ color: '#8e969c' }}
-                iconSize={20}
-                color="white"
-                leadingIcon="logout"
-              />
+              <>
+                <Button
+                  text="Edit Profile"
+                  onClick={() => {
+                    switchWidgets('about');
+                    setShowFocusView(true);
+                  }}
+                  color="white"
+                  height={42}
+                  style={{
+                    fontSize: 13,
+                    color: '#3c3f41',
+                    border: 'none',
+                    marginLeft: 'auto',
+                  }}
+                  leadingIcon={'edit'}
+                  iconSize={15}
+                />
+                <Button
+                  text="Sign out"
+                  onClick={logout}
+                  height={42}
+                  style={{
+                    fontSize: 13,
+                    color: '#3c3f41',
+                    border: 'none',
+                    margin: 0,
+                    padding: 0
+                  }}
+                  iconStyle={{ color: '#8e969c' }}
+                  iconSize={20}
+                  color="white"
+                  leadingIcon="logout"
+                />
+              </>
             ) : (
               <div />
             )}
