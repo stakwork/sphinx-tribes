97126b6ec9824d808687cf5ab20e2e3b56680e64
kevkevin <oapallikunnel@gmail.com>
2023-03-07 15:33:08 -0600

    refactor: moved file to have capital D

--- frontend/app/src/components/common/index.tsx
+++ frontend/app/src/components/common/index.tsx
@@ -2,7 +2,7 @@ import SearchTextInput from './search-text-input';
 import Button from './button';
 import IconButton from './iconButton';
 import Modal from './modal';
-import Divider from './divider';
+import Divider from './Divider';
 import { Title, Date, Paragraph, Link } from './elements';
 import Select from './select';
 import MultiSelect from './multiSelect';
