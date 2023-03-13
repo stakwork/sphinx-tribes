8835ba6c194b3eaa49d6a1b8335c2de46acbcfd9
elraphty <elraphtyofficial@gmail.com>
2023-03-13 14:40:21 +0100

    fixed merge conflicts

--- frontend/app/src/people/personViewSlim.tsx
+++ frontend/app/src/people/personViewSlim.tsx
@@ -276,15 +276,15 @@ export default function PersonView(props: any) {
 
       const panelStyles = isMobile
         ? {
-            minHeight: 132
-          }
+          minHeight: 132
+        }
         : {
-            maxWidth: 291,
-            minWidth: 291,
-            marginRight: 20,
-            marginBottom: 20,
-            minHeight: 472
-          };
+          maxWidth: 291,
+          minWidth: 291,
+          marginRight: 20,
+          marginBottom: 20,
+          minHeight: 472
+        };
 
       fullSelectedWidget &&
         fullSelectedWidget.forEach((s, i) => {
@@ -296,9 +296,9 @@ export default function PersonView(props: any) {
           const conditionalStyles =
             !isMobile && s?.paid
               ? {
-                  border: '1px solid #dde1e5',
-                  boxShadow: 'none'
-                }
+                border: '1px solid #dde1e5',
+                boxShadow: 'none'
+              }
               : {};
 
           elementArray.push(
@@ -558,13 +558,13 @@ export default function PersonView(props: any) {
                 const hasExtras = extras && extras[name] && extras[name].length > 0;
                 const count: any = hasExtras
                   ? extras[name].filter((f) => {
-                      if ('show' in f) {
-                        // show has a value
-                        if (!f.show) return false;
-                      }
-                      // if no value default to true
-                      return true;
-                    }).length
+                    if ('show' in f) {
+                      // show has a value
+                      if (!f.show) return false;
+                    }
+                    // if no value default to true
+                    return true;
+                  }).length
                   : null;
 
                 return (
@@ -620,8 +620,8 @@ export default function PersonView(props: any) {
   function renderDesktopView() {
     const focusedDesktopModalStyles = newSelectedWidget
       ? {
-          ...tabs[newSelectedWidget]?.modalStyle
-        }
+        ...tabs[newSelectedWidget]?.modalStyle
+      }
       : {};
 
     return (
@@ -838,13 +838,13 @@ export default function PersonView(props: any) {
                 const hasExtras = extras && extras[name] && extras[name].length > 0;
                 const count: any = hasExtras
                   ? extras[name].filter((f) => {
-                      if ('show' in f) {
-                        // show has a value
-                        if (!f.show) return false;
-                      }
-                      // if no value default to true
-                      return true;
-                    }).length
+                    if ('show' in f) {
+                      // show has a value
+                      if (!f.show) return false;
+                    }
+                    // if no value default to true
+                    return true;
+                  }).length
                   : null;
 
                 return (
@@ -972,10 +972,9 @@ export default function PersonView(props: any) {
                                 subtitle="Because I'm awesome"
                                 buttonlabel="Donate"
                                 defaultinterval="weekly"
-                                imgurl="${
-                                  mediumPic ||
-                                  'https://i.scdn.co/image/28747994a80c78bc2824c2561d101db405926a37'
-                                }"
+                                imgurl="${mediumPic ||
+              'https://i.scdn.co/image/28747994a80c78bc2824c2561d101db405926a37'
+              }"
                             ></sphinx-widget>`
           }}
         />
