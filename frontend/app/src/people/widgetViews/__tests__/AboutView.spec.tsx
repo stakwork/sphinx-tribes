764d8d469e363213e38053d290bbaccad4b01e97
kevkevin <oapallikunnel@gmail.com>
2023-03-27 11:04:00 -0500

    prettier

--- frontend/app/src/people/widgetViews/__tests__/AboutView.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/AboutView.spec.tsx
@@ -1,36 +1,33 @@
-
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import nock from 'nock';
 import React from 'react';
 import { setupStore } from '__test__/__mockData__/setupStore';
 import { user } from '__test__/__mockData__/user';
 import { mockUsehistory } from '__test__/__mockFn__/useHistory';
-import { AboutView } from "../aboutView";
+import { AboutView } from '../aboutView';
 
 beforeAll(() => {
   nock.disableNetConnect();
   setupStore();
-  mockUsehistory()
+  mockUsehistory();
 });
 
 describe('AboutView Component', () => {
   nock(user.url);
   test('display about view with extras', () => {
     const closeHandler = jest.fn();
-					const description = "test description"
-					  const extras = {
-    email: [{ value: 'testEmail@sphinx.com' }],
-    twitter: [{ value: 'twitterHandle' }],
-    wanted: []
-  }
+    const description = 'test description';
+    const extras = {
+      email: [{ value: 'testEmail@sphinx.com' }],
+      twitter: [{ value: 'twitterHandle' }],
+      wanted: []
+    };
 
-    render(<AboutView extras={extras} description={description} />)
+    render(<AboutView extras={extras} description={description} />);
     expect(screen.queryByText(extras.email[0].value)).toBeInTheDocument();
     expect(screen.queryByText(`@${extras.twitter[0].value}`)).toBeInTheDocument();
     expect(screen.queryByText(description)).toBeInTheDocument();
-  })
-})
-
-
+  });
+});
