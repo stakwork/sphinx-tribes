3c2b4b31202b2745d6752a406f40fb44773203d5
kevkevin <oapallikunnel@gmail.com>
2023-03-29 12:31:30 -0500

    testing if prettier action will go

--- frontend/app/src/config/ModeDispatcher.tsx
+++ frontend/app/src/config/ModeDispatcher.tsx
@@ -24,7 +24,7 @@ const hosts: { [k: string]: AppMode } = {
 function getMode(): AppMode {
   const { host } = window.location;
 
-  return hosts[host] || AppMode.COMMUNITY;
+  return hosts[host] || AppMode.TRIBES;
 }
 
 export const ModeDispatcher = ({
