c79f2e0369a151b31ea7e049b4b7dc33a4ea90c9
elraphty <elraphtyofficial@gmail.com>
2023-03-30 17:02:02 +0100

    fixed assigned tickets modal

--- frontend/app/src/hooks/usePerson.ts
+++ frontend/app/src/hooks/usePerson.ts
@@ -5,9 +5,18 @@ export const usePerson = (id) => {
   const { main, ui } = useStores();
   const { meInfo } = ui || {};
 
-  const person: Person | undefined = (main.people || []).find((f) => f.id === id);
+  let person: Person | undefined;
+
+  if(main.personWanteds.length) {
+    const pid = main.personWanteds[0].person.id;
+    person = (main.people || []).find((f) => f.id === pid);
+    
+  } else {
+    person = (main.people || []).find((f) => f.id === id);
+  }
 
   const canEdit = meInfo?.id === person?.id;
+
   return {
     person: canEdit ? meInfo : person,
     canEdit
