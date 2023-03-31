a3c18e3f865285323455ff50fcd8b8aa45f0c75d
elraphty <elraphtyofficial@gmail.com>
2023-03-31 18:13:47 +0100

    removed unused functions

--- frontend/app/src/people/widgetViews/__tests__/UserTickets.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/UserTickets.spec.tsx
@@ -4,17 +4,14 @@ import nock from 'nock';
 import React from 'react';
 import { setupStore } from '__test__/__mockData__/setupStore';
 import { user } from '__test__/__mockData__/user';
-import { mockUsehistory, mockUseParams, mockUseRouterMatch } from '__test__/__mockFn__/useHistory';
+import { mockUsehistory } from '__test__/__mockFn__/useHistory';
 import UserTicketsView from '../userTicketsView';
 import routeData from 'react-router';
-import { match } from 'react-router-dom';
 
 beforeAll(() => {
     nock.disableNetConnect();
     setupStore();
     mockUsehistory();
-    mockUseParams();
-    mockUseRouterMatch();
 });
 
 describe('UserTicketsView Component', () => {
