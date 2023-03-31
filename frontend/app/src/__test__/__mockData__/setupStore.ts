151d5519af2a9f907ffa84e4e16d51876e5c245a
elraphty <elraphtyofficial@gmail.com>
2023-03-31 16:22:22 +0100

    setup local personWanted

--- frontend/app/src/__test__/__mockData__/setupStore.ts
+++ frontend/app/src/__test__/__mockData__/setupStore.ts
@@ -1,10 +1,11 @@
 import { mainStore } from '../../store/main';
 import { uiStore } from '../../store/ui';
-import { person } from './persons';
+import { person, userTickets } from './persons';
 import { user } from './user';
 
 export const setupStore = () => {
   mainStore.setPeople([person]);
   uiStore.setMeInfo(user);
+  mainStore.setPersonWanteds(userTickets);
   uiStore.setSelectedPerson(user.id as number);
 };
