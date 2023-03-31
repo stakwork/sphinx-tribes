b81df85b2de4fe4d2e5fc7b7d15b7681ebab181b
elraphty <elraphtyofficial@gmail.com>
2023-03-31 18:10:50 +0100

    mocked use params and useroutemacth

--- frontend/app/src/people/widgetViews/__tests__/UserTickets.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/UserTickets.spec.tsx
@@ -4,28 +4,30 @@ import nock from 'nock';
 import React from 'react';
 import { setupStore } from '__test__/__mockData__/setupStore';
 import { user } from '__test__/__mockData__/user';
-import { mockUsehistory } from '__test__/__mockFn__/useHistory';
+import { mockUsehistory, mockUseParams, mockUseRouterMatch } from '__test__/__mockFn__/useHistory';
 import UserTicketsView from '../userTicketsView';
+import routeData from 'react-router';
+import { match } from 'react-router-dom';
 
 beforeAll(() => {
     nock.disableNetConnect();
     setupStore();
     mockUsehistory();
+    mockUseParams();
+    mockUseRouterMatch();
 });
 
 describe('UserTicketsView Component', () => {
     nock(user.url);
-    test('display about view with extras', () => {
-        const description = 'test description';
-        const extras = {
-            email: [{ value: 'testEmail@sphinx.com' }],
-            twitter: [{ value: 'twitterHandle' }],
-            wanted: []
-        };
+    test('display user assigned tickets', () => {
+        jest.spyOn(routeData, 'useParams').mockReturnValue({ personPubKey: 'test_pub_key_2' });
+        jest.spyOn(routeData, 'useRouteMatch').mockReturnValue({ "url": "hello", "path": "jell", "params": {}, "isExact": true });
+
+        const description = 'Just for terst';
+        const title = 'Trying this';
 
         render(<UserTicketsView />);
-        expect(screen.queryByText(extras.email[0].value)).toBeInTheDocument();
-        expect(screen.queryByText(`@${extras.twitter[0].value}`)).toBeInTheDocument();
+        expect(screen.queryByText(title)).toBeInTheDocument();
         expect(screen.queryByText(description)).toBeInTheDocument();
     });
 });
