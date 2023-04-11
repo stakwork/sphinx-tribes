c19f705ecf6d7de35acf2b96ce206c41f6eff2a2
elraphty <elraphtyofficial@gmail.com>
2023-04-11 22:48:57 +0100

    added test for setting lncache

--- db/store_test.go
+++ db/store_test.go
@@ -49,3 +49,27 @@ func TestDeleteCache(t *testing.T) {
 		t.Error("Could not delete cache item")
 	}
 }
+
+func TestSetLnCache(t *testing.T) {
+	var key = "TestLnKey"
+
+	var value = LnStore{
+		K1:     "887775666900000056890P23",
+		Key:    "0000000000000000000000000000000000000",
+		Status: false,
+	}
+
+	InitCache()
+
+	Store.SetLnCache(key, value)
+
+	cacheValue, err := Store.GetLnCache(key)
+
+	if err != nil {
+		t.Error("Cache error thrown")
+	}
+
+	if cacheValue != value {
+		t.Error("Could not set cache item")
+	}
+}
