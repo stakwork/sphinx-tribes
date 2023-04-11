562e5d2bd2b259feb12fc8e88be833021eba7156
elraphty <elraphtyofficial@gmail.com>
2023-04-11 17:23:59 +0100

    added unit test for delete cache

--- db/store_test.go
+++ db/store_test.go
@@ -22,3 +22,30 @@ func TestSetCache(t *testing.T) {
 		t.Error("Could not set cache item")
 	}
 }
+
+func TestDeleteCache(t *testing.T) {
+	var key = "TestKey"
+	var value = "Trial"
+
+	InitCache()
+
+	Store.SetCache(key, value)
+
+	cacheValue, err := Store.GetCache(key)
+
+	if err != nil {
+		t.Error("Cache error thrown")
+	}
+
+	if cacheValue != value {
+		t.Error("Could not set cache item")
+	}
+
+	Store.DeleteCache(key)
+
+	_, errD := Store.GetCache(key)
+
+	if errD == nil {
+		t.Error("Could not delete cache item")
+	}
+}
