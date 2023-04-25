d1db11e907e7950300d5305272cb9d952b0bd0de
Raphael Osaze Eyerin <elraphtyofficial@gmail.com>
2023-04-25 20:31:42 +0100

    Backend Unit Tests And Routes Package (#527)
    * added unit tests for config and jwt
    
    * merged master, and moved the test files to new packages, also updated the Readme with test command
    
    * added store set and get cache test
    
    * added unit test for delete cache
    
    * added test for setting lncache
    
    * tested the ping endpoints
    
    * added test for index route
    
    * started breaking down tests
    
    * moved routes.go to routes package
    
    * merged master and moved leaderboard endpoint to people route file
    
    * added go test for pull request
    
    * successfully setup mock
    
    * fixed git actions errror
    
    * passed connectioncodes test
    
    * made headways with DB TEST
    
    * added routes and changed hosts to default

---
+++ db/store_test.go
@@ -0,0 +1,75 @@
+package db
+
+import (
+	"testing"
+)
+
+func TestSetCache(t *testing.T) {
+	var key = "TestKey"
+	var value = "Trial"
+
+	InitCache()
+
+	Store.SetCache(key, value)
+
+	cacheValue, err := Store.GetCache(key)
+
+	if err != nil {
+		t.Error("Cache error thrown")
+	}
+
+	if cacheValue != value {
+		t.Error("Could not set cache item")
+	}
+}
+
+func TestDeleteCache(t *testing.T) {
+	var key = "TestKey"
+	var value = "Trial"
+
+	InitCache()
+
+	Store.SetCache(key, value)
+
+	cacheValue, err := Store.GetCache(key)
+
+	if err != nil {
+		t.Error("Cache error thrown")
+	}
+
+	if cacheValue != value {
+		t.Error("Could not set cache item")
+	}
+
+	Store.DeleteCache(key)
+
+	_, errD := Store.GetCache(key)
+
+	if errD == nil {
+		t.Error("Could not delete cache item")
+	}
+}
+
+func TestSetLnCache(t *testing.T) {
+	var key = "TestLnKey"
+
+	var value = LnStore{
+		K1:     "887775666900000056890P23",
+		Key:    "0000000000000000000000000000000000000",
+		Status: false,
+	}
+
+	InitCache()
+
+	Store.SetLnCache(key, value)
+
+	cacheValue, err := Store.GetLnCache(key)
+
+	if err != nil {
+		t.Error("Cache error thrown")
+	}
+
+	if cacheValue != value {
+		t.Error("Could not set cache item")
+	}
+}
