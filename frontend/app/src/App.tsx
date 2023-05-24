6dc809b6cc7d03c363b617d7449b59ce8245ae5e
elraphty <elraphtyofficial@gmail.com>
2023-05-24 17:35:27 +0100

    changed config to default

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -8,7 +8,6 @@ import { ModeDispatcher } from './config/ModeDispatcher';
 import { Pages } from './pages';
 import { mainStore } from './store/main';
 import history from 'config/history';
-import { socket } from 'config/socket';
 
 let exchangeRateInterval: any = null;
 
