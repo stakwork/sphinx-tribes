e300befd4984f78ae3c95438668bde04680349f2
elraphty <elraphtyofficial@gmail.com>
2023-06-22 16:08:29 +0100

    fixed leaderboard error

--- frontend/app/src/leaderboard/store.ts
+++ frontend/app/src/leaderboard/store.ts
@@ -25,7 +25,7 @@ export class LeaderboardStore {
     try {
       const resp = (await api.get('people/bounty/leaderboard')) as LeaderItem[];
       this.total = resp[0];
-      this.leaders = resp.slice(1);
+      this.leaders = resp;
     } catch (e) {
       this.error = e;
     } finally {
