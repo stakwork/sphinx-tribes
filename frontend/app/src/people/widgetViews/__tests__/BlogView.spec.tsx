f8b83a7cd9ec05afc7f527167e4075fb67b95204
elraphty <elraphtyofficial@gmail.com>
2023-07-12 14:40:43 +0100

    added support me view tests

--- frontend/app/src/people/widgetViews/__tests__/BlogView.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/BlogView.spec.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import nock from 'nock';
 import React from 'react';
 import { setupStore } from '__test__/__mockData__/setupStore';
-import { user } from '__test__/__mockData__/user';
 import { mockUsehistory } from '__test__/__mockFn__/useHistory';
 import BlogView from '../BlogView';
 import { blowViews } from '__test__/__mockData__/blogViews';
@@ -16,7 +15,6 @@ beforeAll(() => {
 
 // Todo : mock api request in usertickets page
 describe('BlogView Component', () => {
-    nock(user.url);
     test('It showed display blog title', async () => {
         render(<BlogView {...blowViews} />);
         expect(screen.queryByText(blowViews.title)).toBeInTheDocument();
