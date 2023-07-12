6f564db65b803a39d32f8efdc355aa07f3a9103a
elraphty <elraphtyofficial@gmail.com>
2023-07-12 14:14:07 +0100

    added TwitterView test

---
+++ frontend/app/src/people/widgetViews/__tests__/TwitterView.spec.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import nock from 'nock';
+import React from 'react';
+import { setupStore } from '__test__/__mockData__/setupStore';
+import { user } from '__test__/__mockData__/user';
+import { mockUsehistory } from '__test__/__mockFn__/useHistory';
+import { blowViews } from '__test__/__mockData__/blogViews';
+import TwitterView from '../TwitterView';
+
+beforeAll(() => {
+    nock.disableNetConnect();
+    setupStore();
+    mockUsehistory();
+});
+
+// Todo : mock api request in usertickets page
+describe('TwitterView Component', () => {
+    nock(user.url);
+    test('It showed display twitter handle', async () => {
+        render(<TwitterView handle='test_test' />);
+        expect(screen.queryByText('@test_test')).toBeInTheDocument();
+    });
+});
