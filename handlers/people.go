9dc22dc7eb6b6da9f87ccde7c16985fee35fa62f
kevkevin <oapallikunnel@gmail.com>
2023-07-13 10:21:31 -0500

    Revert "Feat/bounty migration (#576)" (#585)
    This reverts commit aaf265d9296d7143898f97647692479de4df1065.

--- handlers/people.go
+++ handlers/people.go
@@ -51,6 +51,7 @@ func CreateOrEditPerson(w http.ResponseWriter, r *http.Request) {
 	}
 
 	existing := db.DB.GetPersonByPubkey(pubKeyFromAuth)
+	fmt.Println("Existing ===", existing)
 	if existing.ID == 0 { // new!
 		if person.ID != 0 { // cant try to "edit" if not exists already
 			fmt.Println("cant edit non existing")
