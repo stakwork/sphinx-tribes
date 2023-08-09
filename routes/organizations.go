ca68ca9c730bace77c7f7db4b87a047f0575bf14
elraphty <elraphtyofficial@gmail.com>
2023-08-09 20:48:24 +0100

    added organizations get routes

---
+++ routes/organizations.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/stakwork/sphinx-tribes/auth"
+	"github.com/stakwork/sphinx-tribes/handlers"
+)
+
+func OrganizationRoutes() chi.Router {
+	r := chi.NewRouter()
+
+	r.Group(func(r chi.Router) {
+		r.Get("/", handlers.GetOrganizations)
+		r.Get("/{uuid}", handlers.GetOrganizationByUuid)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(auth.PubKeyContext)
+
+		r.Post("/", handlers.CreateOrEditOrganization)
+	})
+
+	return r
+}
