c3cab263452d79a1b50e6d25bc7a79e445fe1baf
elraphty <elraphtyofficial@gmail.com>
2023-08-21 23:31:45 +0100

    add organization button

--- frontend/app/src/people/widgetViews/OrganizationView.tsx
+++ frontend/app/src/people/widgetViews/OrganizationView.tsx
@@ -4,11 +4,13 @@ import PageLoadSpinner from 'people/utils/PageLoadSpinner';
 import NoResults from 'people/utils/OrgNoResults';
 import { useStores } from 'store';
 import { Organization } from 'store/main';
+import { IconButton } from 'components/common';
+import { useIsMobile } from 'hooks/uiHooks';
 import avatarIcon from '../../public/static/profile_avatar.svg';
 
 const Container = styled.div`
   display: flex;
-  flex-flow: row wrap;
+  flex-flow: column wrap;
   gap: 1rem;
   min-width: 77vw;
   flex: 1 1 100%;
@@ -31,11 +33,19 @@ const OrganizationWrap = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  width: calc(19% - 40px);
+  margin-left: 0.5%;
+  margin-right: 0.5%;
+  margin; 10px;
+  background: white;
+  padding: 20px;
+  border-radius: 2px;
 `;
 
 const Organizations = () => {
     const [loading, setIsLoading] = useState<boolean>(false);
     const { main } = useStores();
+    const isMobile = useIsMobile();
 
     async function getUserOrganizations() {
         setIsLoading(true);
@@ -66,6 +76,15 @@ const Organizations = () => {
         <div>
             <Container>
                 <PageLoadSpinner show={loading} />
+                <IconButton
+                    width={150}
+                    height={isMobile ? 36 : 48}
+                    text="Add Organization"
+                    onClick={() => false}
+                    style={{
+                        marginLeft: '10px'
+                    }}
+                />
                 {renderOrganizations()}
             </Container>
         </div>
