781af768c2cc32ddc69e710fcb70f687c719fccd
kevkevin <oapallikunnel@gmail.com>
2023-08-29 09:23:00 -0500

    make delete bounty an authenticated route

--- routes/bounty.go
+++ routes/bounty.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/stakwork/sphinx-tribes/auth"
 	"github.com/stakwork/sphinx-tribes/handlers"
 )
 
@@ -11,10 +12,16 @@ func BountyRoutes() chi.Router {
 	r.Group(func(r chi.Router) {
 		r.Post("/", handlers.CreateOrEditBounty)
 		r.Delete("/assignee", handlers.DeleteBountyAssignee)
-		r.Delete("/{pubKey}/{created}", handlers.DeleteBounty)
 		r.Get("/all", handlers.GetAllBounties)
 		r.Get("/id/{bountyId}", handlers.GetBountyById)
 		r.Get("/count/{personKey}/{tabType}", handlers.GetBountyCount)
 	})
+
+	r.Group(func(r chi.Router) {
+		r.Use(auth.PubKeyContext)
+
+		r.Delete("/{pubKey}/{created}", handlers.DeleteBounty)
+	})
+
 	return r
 }
