682a3681b2cff1f173825be4f408cc3cbed862d1
VladimirZhigalev <maradondt@gmail.com>
2023-09-14 23:14:31 +0200

    fix: fix share to twitter handler (#677)
    * fix: fix share to twitter handler
    
    * fix: tweet content

--- frontend/app/src/people/widgetViews/summaries/WantedSummary.tsx
+++ frontend/app/src/people/widgetViews/summaries/WantedSummary.tsx
@@ -12,7 +12,7 @@ import { useStores } from '../../../store';
 import { LanguageObject, awards } from '../../utils/languageLabelStyle';
 import NameTag from '../../utils/NameTag';
 import { sendToRedirect } from '../../../helpers';
-import { WantedSummaryProps } from '../../interfaces';
+import { CodingLanguageLabel, WantedSummaryProps } from '../../interfaces';
 import CodingMobile from './wantedSummaries/CodingMobile';
 import CodingBounty from './wantedSummaries/CodingBounty';
 import CodingDesktop from './wantedSummaries/CodingDesktop';
@@ -110,7 +110,7 @@ function WantedSummary(props: WantedSummaryProps) {
 
   const isMine = ui.meInfo?.owner_pubkey === person?.owner_pubkey;
 
-  const [labels, setLabels] = useState([]);
+  const [labels, setLabels] = useState<Array<CodingLanguageLabel>>([]);
   const [assigneeValue, setAssigneeValue] = useState(false);
 
   const assigneeHandlerOpen = () => setAssigneeValue((assigneeValue: any) => !assigneeValue);
@@ -146,7 +146,7 @@ function WantedSummary(props: WantedSummaryProps) {
         description: description,
         price: price,
         assignee: value?.owner_pubkey || '',
-        coding_language: coding_languages?.map((x: any) => ({ label: x, value: x })),
+        coding_language: coding_languages?.map((x: string) => ({ label: x, value: x })),
         estimated_session_length: estimated_session_length,
         show: show,
         type: type,
@@ -192,11 +192,10 @@ function WantedSummary(props: WantedSummaryProps) {
     let res;
     if (coding_languages?.length > 0) {
       res = LanguageObject?.filter(
-        (value: any) => coding_languages?.find((val: any) => val === value.label)
+        (value: any) => coding_languages?.find((val: string) => val === value.label)
       );
     }
     setDataValue(res);
-    setLabels(res);
   }, [coding_languages]);
 
   const searchParams = useQuery();
@@ -210,7 +209,7 @@ function WantedSummary(props: WantedSummaryProps) {
 
   useEffect(() => {
     if (coding_languages) {
-      const values = coding_languages.map((value: any) => ({ label: value, value: value }));
+      const values = coding_languages.map((value: string) => ({ label: value, value: value }));
       setLabels(values);
     }
   }, [coding_languages]);
@@ -543,6 +542,7 @@ function WantedSummary(props: WantedSummaryProps) {
       return (
         <CodingBounty
           {...props}
+          labels={labels}
           person={person}
           awardDetails={awardDetails}
           setAwardDetails={setAwardDetails}
@@ -589,13 +589,13 @@ function WantedSummary(props: WantedSummaryProps) {
       <div>
         <CodingDesktop
           {...props}
+          labels={labels}
           actionButtons={actionButtons}
           nametag={nametag}
           assigneeLabel={assigneeLabel}
           assignee={assignee}
           loomEmbedUrl={loomEmbedUrl}
           titleString={titleString}
-          // status={status}
           handleCopyUrl={handleCopyUrlProfilePage}
           isCopied={isCopied}
         />
---
+++ frontend/app/src/people/widgetViews/summaries/wantedSummaries/lib.ts
@@ -0,0 +1,35 @@
+import { CodingLanguageLabel } from 'people/interfaces';
+
+type Props = {
+  title: string;
+  labels?: Array<CodingLanguageLabel>;
+  ownerPubkey: string;
+  issueCreated: string;
+};
+export const getTwitterLink = ({ title, issueCreated, ownerPubkey, labels }: Props) => {
+  const bountyParams = {
+    owner_id: ownerPubkey,
+    created: issueCreated
+  };
+  const bountyUrl = new URL('https://community.sphinx.chat/tickets');
+
+  for (const key in bountyParams) {
+    bountyUrl.searchParams.append(key, bountyParams[key]);
+  }
+
+  const params = {
+    text: `I just created a bounty on Sphinx Community: ${title}\n`,
+    url: `${bountyUrl}\n`,
+    hashtags: [...(labels ?? []), { label: 'sphinxchat', value: '' }]
+    .map((x: CodingLanguageLabel) => x.label)
+    .join(','),
+  };
+
+  const link = new URL('https://twitter.com/intent/tweet');
+
+  for (const key in params) {
+    link.searchParams.append(key, params[key]);
+  }
+
+  return link.toString();
+};
