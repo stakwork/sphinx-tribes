491c3c918a9141d1137a9c722588331732299df6
Evan Feenstra <evanfeenstra@gmail.com>
2023-09-27 09:03:10 -0700

    get tribes by app url (#746)

--- routes/tribes.go
+++ routes/tribes.go
@@ -10,6 +10,8 @@ func TribeRoutes() chi.Router {
 
 	r.Group(func(r chi.Router) {
 		r.Get("/", handlers.GetListedTribes)
+		r.Get("/app_url/{app_url}", handlers.GetTribesByAppUrl)
+		r.Get("/app_urls/{app_urls}", handlers.GetTribesByAppUrls)
 		r.Get("/{uuid}", handlers.GetTribe)
 		r.Get("/total", handlers.GetTotalribes)
 		r.Post("/", handlers.CreateOrEditTribe)
