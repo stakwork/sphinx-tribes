3aec26fd7ec1ec0233ac7fd60abdef7b358c3127
elraphty <elraphtyofficial@gmail.com>
2023-10-13 13:24:06 +0100

    ran prettier

--- frontend/app/src/people/widgetViews/DeleteModal.tsx
+++ frontend/app/src/people/widgetViews/DeleteModal.tsx
@@ -28,26 +28,32 @@ const UserImage = styled.img`
   z-index: 200;
   position: absolute;
   margin-top: -24%;
-  border: 4px solid #FFF;
+  border: 4px solid #fff;
 `;
 
 const CancelButton = styled(EuiButton)`
   color: #000;
-  background: #FFF;
-  border: 1px solid #D0D5D8;
+  background: #fff;
+  border: 1px solid #d0d5d8;
   text-decoration: none !important;
   box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.06);
 `;
 
 const DeleteButton = styled(EuiButton)`
-  background: #ED7474;
-  color: #FFFFFF;
+  background: #ed7474;
+  color: #ffffff;
   border: none;
   text-decoration: none !important;
-  box-shadow: 0px 2px 10px 0px rgba(237, 116, 116, 0.50);
+  box-shadow: 0px 2px 10px 0px rgba(237, 116, 116, 0.5);
 `;
 
-const DeleteTicketModal = ({ closeModal, confirmDelete, text, imgUrl, userDelete }: DeleteTicketModalProps) => (
+const DeleteTicketModal = ({
+  closeModal,
+  confirmDelete,
+  text,
+  imgUrl,
+  userDelete
+}: DeleteTicketModalProps) => (
   <EuiOverlayMask>
     <EuiModal
       onClose={closeModal}
@@ -56,26 +62,14 @@ const DeleteTicketModal = ({ closeModal, confirmDelete, text, imgUrl, userDelete
         padding: '50px 50px 30px 50px'
       }}
     >
-      {
-        userDelete && (
-          <UserImage src={imgUrl || avatarIcon} />
-        )
-      }
+      {userDelete && <UserImage src={imgUrl || avatarIcon} />}
       <WarningText>
         Are you sure you want to <br />
         <BoldText>Delete this {text ? text : 'Ticket'}?</BoldText>
       </WarningText>
       <ModalButtonContainer>
-        <CancelButton
-          onClick={closeModal}
-        >
-          Cancel
-        </CancelButton>
-        <DeleteButton
-          onClick={confirmDelete}
-        >
-          Delete
-        </DeleteButton>
+        <CancelButton onClick={closeModal}>Cancel</CancelButton>
+        <DeleteButton onClick={confirmDelete}>Delete</DeleteButton>
       </ModalButtonContainer>
     </EuiModal>
   </EuiOverlayMask>
