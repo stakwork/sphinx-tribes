01b20d3542a4c9287e17233e2381ff769d3d2cbb
Mac S <54864515+cosmicpotato137@users.noreply.github.com>
2023-11-15 10:49:15 -0500

    Unit tests for filters (#940)
    * update user roles update window
    
    * final tweaks
    
    * syncing with origin
    
    * added filterValidation tests
    
    * finished tests for bountyHeaderLanguageFilter, bountyHeaderFilter functions
    
    * prettier
    
    * bug fix
    
    * fixed a/t/t test
    
    * test: fixed test to pass
    
    * fix: no new yarn.lock
    
    ---------
    
    Co-authored-by: kevkevin <oapallikunnel@gmail.com>

---
+++ frontend/app/src/people/widgetViews/__tests__/WidgetSwitchViewer.spec.tsx
@@ -0,0 +1,10 @@
+import WidgetViewer from '../WidgetSwitchViewer';
+import { uiStore } from '../../../store/ui';
+// import crypto from 'crypto';
+// import moment from 'moment';
+
+beforeAll(() => {});
+
+afterAll(() => {});
+
+describe('testing helpers', () => {});
