e1c6da7ea841ed481cd9b0da96cfb0a87adef7a2
elraphty <elraphtyofficial@gmail.com>
2023-11-23 20:27:18 +0100

    added bounty metrics

--- db/metrics.go
+++ db/metrics.go
@@ -1,11 +1,63 @@
 package db
 
+import "math"
+
 func (db database) TotalPaymentsByDateRange(r PaymentDateRange) uint {
 	var sum uint
 
 	db.db.Model(&PaymentHistory{}).Where("payment_type = ?", r.PaymentType).Where("created >= ?", r.StartDate).Where("created <= ?", r.EndDate).Select("SUM(amount)").Row().Scan(&sum)
 
 	return sum
+}
+
+func (db database) TotalSatsPaid(r PaymentDateRange) uint {
+	var sum uint
+
+	db.db.Model(&Bounty{}).Where("paid_date >= ?", r.StartDate).Where("paid_date <= ?", r.EndDate).Select("SUM(amount)").Row().Scan(&sum)
+
+	return sum
+}
+
+func (db database) TotalSatsPosted(r PaymentDateRange) uint {
+	var sum uint
+
+	db.db.Model(&Bounty{}).Where("created >= ?", r.StartDate).Where("created <= ?", r.EndDate).Select("SUM(amount)").Row().Scan(&sum)
+
+	return sum
+}
+
+func (db database) SatsPaidPercentage(r PaymentDateRange) uint {
+	satsPosted := DB.TotalSatsPosted(r)
+	satsPaid := DB.TotalSatsPaid(r)
+
+	value := satsPosted * 100 / satsPaid
+	paidPercentage := math.Round(float64(value))
+	return uint(paidPercentage)
+}
+
+func (db database) TotalPaidBounties(r PaymentDateRange) int64 {
+	var count int64
+
+	db.db.Model(&Bounty{}).Where("paid_date >= ?", r.StartDate).Where("paid_date <= ?", r.EndDate).Count(&count)
+
+	return count
+}
+
+func (db database) TotalBountiesPosted(r PaymentDateRange) int64 {
+	var count int64
+
+	db.db.Model(&Bounty{}).Where("created >= ?", r.StartDate).Where("created <= ?", r.EndDate).Count(&count)
+
+	return count
+}
+
+func (db database) BountiesPaidPercentage(r PaymentDateRange) uint {
+	bountiesPosted := DB.TotalBountiesPosted(r)
+	bountiesPaid := DB.TotalPaidBounties(r)
+
+	value := bountiesPaid * 100 / bountiesPosted
+	paidPercentage := math.Round(float64(value))
+	return uint(paidPercentage)
 }
 
 func (db database) TotalPeopleByDateRange(r PaymentDateRange) int64 {
--- handlers/metrics.go
+++ handlers/metrics.go
@@ -90,3 +90,44 @@ func PeopleMetrics(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(sumAmount)
 }
+
+func BountyMetrics(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	pubKeyFromAuth, _ := ctx.Value(auth.ContextKey).(string)
+
+	if pubKeyFromAuth == "" {
+		fmt.Println("no pubkey from auth")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	request := db.PaymentDateRange{}
+	body, err := io.ReadAll(r.Body)
+	r.Body.Close()
+
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		json.NewEncoder(w).Encode("Request body not accepted")
+		return
+	}
+
+	totalBountiesPosted := db.DB.TotalBountiesPosted(request)
+	totalBountiesPaid := db.DB.TotalPaidBounties(request)
+	bountiesPaidPercentage := db.DB.BountiesPaidPercentage(request)
+	totalSatsPosted := db.DB.TotalSatsPosted(request)
+	totalSatsPaid := db.DB.TotalSatsPaid(request)
+	satsPaidPercentage := db.DB.SatsPaidPercentage(request)
+
+	bountyMetrics := db.BountyMetrics{
+		BountiesPosted:         totalBountiesPosted,
+		BountiesPaid:           totalBountiesPaid,
+		BountiesPaidPercentage: bountiesPaidPercentage,
+		SatsPosted:             totalSatsPosted,
+		SatsPaid:               totalSatsPaid,
+		SatsPaidPercentage:     satsPaidPercentage,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(bountyMetrics)
+}
