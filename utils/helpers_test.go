ac2be13699659d064d71e52eb10d72caaaaf3d82
elraphty <elraphtyofficial@gmail.com>
2023-11-29 13:58:27 +0100

    added more test for the difference function

--- utils/helpers_test.go
+++ utils/helpers_test.go
@@ -9,8 +9,20 @@ import (
 
 func TestGetDateDaysDifference(t *testing.T) {
 	created := 1700238956
+	exactTime := time.Now().Unix()
 
 	testDate := time.Now()
+	nextDate := time.Now().AddDate(0, 0, 2)
 	days := GetDateDaysDifference(int64(created), &testDate)
+
 	assert.NotEqual(t, 0, days)
+
+	daysEqual := GetDateDaysDifference(exactTime, &testDate)
+	assert.Equal(t, int64(0), daysEqual)
+
+	daysNext := GetDateDaysDifference(exactTime, &nextDate)
+	assert.Equal(t, int64(2), daysNext)
+
+	wrongDate := GetDateDaysDifference(0, &nextDate)
+	assert.Greater(t, wrongDate, int64(365))
 }
