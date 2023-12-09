f79d408fcaf938182ec30a7c209af76e3c54f3b8
Vividh Pandey <vividh.pandey.13@gmail.com>
2023-12-09 19:36:26 +0530

    Fixed eslint issues
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- frontend/app/src/pages/people/PersonPage.tsx
+++ frontend/app/src/pages/people/PersonPage.tsx
@@ -9,6 +9,7 @@ import { useStores } from 'store';
 import styled from 'styled-components';
 import { TabsPages } from './tabs';
 
+// eslint-disable-next-line @typescript-eslint/no-inferrable-types
 const getHtml = (owner_pubkey: string = '', img: string = '') => `
 <sphinx-widget pubkey=${owner_pubkey}
   amount="500"
