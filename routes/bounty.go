78df24d687db2656e15e87a41e926323a9dc3405
elraphty <elraphtyofficial@gmail.com>
2023-12-11 11:29:47 +0100

    fixed some bounties not displaying

--- routes/bounty.go
+++ routes/bounty.go
@@ -11,6 +11,7 @@ func BountyRoutes() chi.Router {
 	r.Group(func(r chi.Router) {
 		r.Get("/all", handlers.GetAllBounties)
 		r.Get("/id/{bountyId}", handlers.GetBountyById)
+		r.Get("/index/{bountyId}", handlers.GetBountyIndexById)
 		r.Get("/created/{created}", handlers.GetBountyByCreated)
 		r.Get("/count/{personKey}/{tabType}", handlers.GetUserBountyCount)
 		r.Get("/count", handlers.GetBountyCount)
