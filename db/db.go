20a86e34bab3c5f8aacdb7ee130589ed8ec37210
kevkevin <oapallikunnel@gmail.com>
2023-12-14 00:19:01 -0600

    added mocks and new tests mocking the db for the handlers

--- db/db.go
+++ db/db.go
@@ -1,3 +1,6 @@
+//go:build !mock
+// +build !mock
+
 package db
 
 import (
--- handlers/auth_test.go
+++ handlers/auth_test.go
@@ -32,3 +32,33 @@ func TestGetAdminPubkeys(t *testing.T) {
 		t.Errorf("handler returned unexpected body: expected %s pubkeys %s is there a space after?", expected, rr.Body.String())
 	}
 }
+
+func TestGetConnectionCode(t *testing.T) {
+	//origDBGetConnectionCode := db.DB
+	//defer func() { db.DB = origDBGetConnectionCode }()
+	//db.DB = func() {
+	//	// Insert fake implementation here
+	//	functions := func() GetConnectionCode { return "string" }
+	//	return  functions
+	//}
+	req, err := http.NewRequest("GET", "/health-check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetConnectionCode)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"connection_string":"test","date_created":null}`
+	if strings.TrimRight(rr.Body.String(), "\n") != expected {
+
+		t.Errorf("handler returned unexpected body: expected %s pubkeys %s is there a space after?", expected, rr.Body.String())
+	}
+
+}
