164d8098e107f04451318573cb48794118aece72
kevkevin <oapallikunnel@gmail.com>
2023-12-14 00:34:52 -0600

    createConnectionCode test added

--- handlers/auth_test.go
+++ handlers/auth_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -31,6 +32,31 @@ func TestGetAdminPubkeys(t *testing.T) {
 
 		t.Errorf("handler returned unexpected body: expected %s pubkeys %s is there a space after?", expected, rr.Body.String())
 	}
+}
+
+func TestCreateConnectionCode(t *testing.T) {
+
+	var jsonStr = []byte(`{"id":0,"connection_string":"string","is_used":false,"date_created":"2015-09-15T11:50:00Z"}`)
+	req, err := http.NewRequest("POST", "/health-check", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CreateConnectionCode)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := ``
+	if strings.TrimRight(rr.Body.String(), "\n") != expected {
+
+		t.Errorf("handler returned unexpected body: expected %s pubkeys %s is there a space after?", expected, rr.Body.String())
+	}
+
 }
 
 func TestGetConnectionCode(t *testing.T) {
