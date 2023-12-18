28cdbd445d86aca8c7177814cc27b8d16aec9013
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2023-12-18 23:52:12 +0530

    fixes for Varays master #1080 (#1085)
    * fix(#1015): implemented the frontend for superadmin
    
    * fix(#1015): implemented the frontend for superadmin
    
    * Implemented The frontend for superadmin #1051
    
    * implemented the frontend for superadmin #1050
    
    * changes for Varays master #1080
    
    * fix for failing test #1080
    
    * fixed routes
    
    * Capitalized File name and removed auth folder
    
    * moved headerstyles -> HeaderStyles
    
    * prettier
    
    * minor fixes
    
    * modified format and am showing header on super admin page
    
    * remove yarn.lock on top level
    
    ---------
    
    Co-authored-by: kevkevin <oapallikunnel@gmail.com>

---
+++ frontend/app/src/pages/superadmin/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { MyTable } from './TableComponent';
+import { bounties } from './TableComponent/mockBountyData';
+import { Header } from './Header';
+import { Statistics } from './Statistics';
+
+const Container = styled.body`
+  height: 100vh; /* Set a fixed height for the container */
+  overflow-y: auto; /* Enable vertical scrolling */
+  width: 1366px;
+  align-items: center;
+  margin: 0px auto;
+`;
+
+export const SuperAdmin = () => (
+  <Container>
+    <Header />
+    <Statistics />
+    <MyTable bounties={bounties} />
+  </Container>
+);
