8b8e2e71bb8146fa403373a8d48e93c5ae2ac02a
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-12-20 19:22:17 +0100

    feat:width when adding a user to an org is initially incorrect

---
+++ frontend/app/src/people/widgetViews/__tests__/AddUserModal.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import AddUserModal from '../organization/AddUserModal';
+import { AddUserModalProps } from '../organization/interface';
+
+describe('Width of Modal remains the same before and after input', () => {
+  const MockProps: AddUserModalProps = {
+    loading: false,
+    onSubmit: jest.fn(),
+    disableFormButtons: false,
+    setDisableFormButtons: jest.fn(),
+    isOpen: true,
+    close: jest.fn()
+  };
+
+  it('should retain the same width before and after inputting text', () => {
+    render(<AddUserModal {...MockProps} />);
+
+    const modal = screen.getByRole('dialog');
+
+    const initialWidth = modal.offsetWidth;
+
+    const input = screen.getByRole('textbox');
+
+    fireEvent.change(input, { target: { value: 'Saif' } });
+
+    const widthAfterInput = modal.offsetWidth;
+
+    expect(initialWidth).toBe(widthAfterInput);
+  });
+});
