82f82baca3da8bf48b9bf6f047f29b8d5f5eb6a6
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2023-12-22 19:02:34 +0530

    fix:fix for build

--- frontend/app/src/config/host.ts
+++ frontend/app/src/config/host.ts
@@ -2,9 +2,9 @@ const internalDockerHosts = ['localhost:13007', 'localhost:13000'];
 const externalDockerHosts = ['localhost:23007', 'localhost:23000'];
 
 export function getHost(): string {
-  // const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
-  // rerturn host;
-  return "people.sphinx.chat";
+  const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
+  return host;
+
 }
 
 export function getHostIncludingDockerHosts() {
