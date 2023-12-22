7c939552c60238a47bac17f26d91c92e6da78a23
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2023-12-22 18:35:08 +0530

    bug(#1029):Removed the small white module

--- frontend/app/src/config/host.ts
+++ frontend/app/src/config/host.ts
@@ -2,8 +2,9 @@ const internalDockerHosts = ['localhost:13007', 'localhost:13000'];
 const externalDockerHosts = ['localhost:23007', 'localhost:23000'];
 
 export function getHost(): string {
-  const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
-  return host;
+  // const host = window.location.host.includes('localhost') ? 'localhost:5002' : window.location.host;
+  // rerturn host;
+  return "people.sphinx.chat";
 }
 
 export function getHostIncludingDockerHosts() {
