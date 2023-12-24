128eb1b17fd5014141f40e51d83cd9c22a27e1d2
Gourav Kadu <gouravmpk@gmail.com>
2023-12-24 13:35:58 +0530

    REMOVED commented part

--- frontend/app/src/people/widgetViews/parts/StatusPill.tsx
+++ frontend/app/src/people/widgetViews/parts/StatusPill.tsx
@@ -2,39 +2,6 @@ import { StatusPillProps } from 'people/interfaces';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-// interface PillProps {
-//   readonly isOpen: boolean;
-// }
-// const Pill = styled.div<PillProps>`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   font-size: 12px;
-//   font-weight: 300;
-//   background: ${(p: any) => (p.isOpen ? '#49C998' : '#8256D0')};
-//   border-radius: 30px;
-//   border: 1px solid transparent;
-//   text-transform: capitalize;
-//   padding: 12px 5px;
-//   // padding:8px;
-//   font-size: 12px;
-//   font-weight: 500;
-//   line-height: 20px;
-//   white-space: nowrap;
-//   border-radius: 2em;
-//   height: 26px;
-//   color: #fff;
-//   margin-right: 10px;
-//   width: 58px;
-//   height: 22px;
-//   left: 19px;
-//   top: 171px;
-
-//   /* Primary Green */
-
-//   border-radius: 2px;
-// `;
-
 const Assignee = styled.div`
   display: flex;
   justify-content: center;
