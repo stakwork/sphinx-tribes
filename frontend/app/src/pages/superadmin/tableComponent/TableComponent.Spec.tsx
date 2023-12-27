77e8e887b8e0edd3829a5adc6e53c3c438c38597
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2023-12-27 15:53:34 +0100

    fix: fixed prettier warnings

--- frontend/app/src/pages/superadmin/tableComponent/TableComponent.Spec.tsx
+++ frontend/app/src/pages/superadmin/tableComponent/TableComponent.Spec.tsx
@@ -20,82 +20,82 @@ const mockBounties = [
 ];
 
 it('renders elements from TableProps in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText(mockBounties[0].title)).toBeInTheDocument();
 });
 
 it('renders "Sort By:" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Sort By:')).toBeInTheDocument();
 });
 
 it('renders "Date" twice in the document', () => {
-  const { getAllByText } = render(<MyTable bounties={mockBounties} />);
+  const { getAllByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getAllByText('Date')).toHaveLength(2);
 });
 
 it('renders "Assignee" twice in the document', () => {
-  const { getAllByText } = render(<MyTable bounties={mockBounties} />);
+  const { getAllByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getAllByText('Assignee')).toHaveLength(2);
 });
 
 it('renders "Status" twice in the document', () => {
-  const { getAllByText } = render(<MyTable bounties={mockBounties} />);
+  const { getAllByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getAllByText('Status')).toHaveLength(2);
 });
 
 it('renders "Status:" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Status:')).toBeInTheDocument();
 });
 
 it('renders "All" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('All')).toBeInTheDocument();
 });
 
 it('renders "Open" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Open')).toBeInTheDocument();
 });
 
 it('renders "In Progress" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('In Progress')).toBeInTheDocument();
 });
 
 it('renders "Completed" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Completed')).toBeInTheDocument();
 });
 
 it('renders "Bounty" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Bounty')).toBeInTheDocument();
 });
 
 it('renders "#DTGP" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('#DTGP')).toBeInTheDocument();
 });
 
 it('renders "Provider" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Provider')).toBeInTheDocument();
 });
 
 it('renders "Organization" in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText('Organization')).toBeInTheDocument();
 });
 
 it('renders each element in the table in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   expect(getByText(mockBounties[0].title)).toBeInTheDocument();
 });
 
 it('renders each element in the table in the document', () => {
-  const { getByText } = render(<MyTable bounties={mockBounties} />);
+  const { getByText } = render(<MyTable bounties={mockBounties} headerIsFrozen={false} />);
   mockBounties.forEach((bounty) => {
     expect(getByText(bounty.title)).toBeInTheDocument();
     expect(getByText(bounty.date)).toBeInTheDocument();
