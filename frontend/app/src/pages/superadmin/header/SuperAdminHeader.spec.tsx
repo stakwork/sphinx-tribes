f9995208e739f0bec8ccab0c2f5ec97763d2ac69
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-03 01:24:17 +0530

    fix for test

--- frontend/app/src/pages/superadmin/header/SuperAdminHeader.spec.tsx
+++ frontend/app/src/pages/superadmin/header/SuperAdminHeader.spec.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import moment from 'moment';
 import nock from 'nock';
 import React from 'react';
@@ -18,44 +18,66 @@ beforeAll(() => {
  * @jest-environment jsdom
  */
 describe('Header Component', () => {
+  beforeEach(() => {
+    nock.cleanAll();
+  });
+
   nock(user.url).get('/person/id/1').reply(200, {});
 
-  test('display header with extras', async () => {
+  test('displays header with extras', () => {
     const setStartDateMock = jest.fn();
     const setEndDateMock = jest.fn();
     const exportCSVText = 'Export CSV';
-    const initDateRange = '7 Days';
+    const initDateRange = 'Last 7 Days';
 
     render(
       <Header
-        startDate={1703969172}
-        endDate={1700073000}
+        startDate={moment().subtract(7, 'days').startOf('day').unix()}
+        endDate={moment().startOf('day').unix()}
         setStartDate={setStartDateMock}
         setEndDate={setEndDateMock}
       />
     );
 
-    const dateRangeRegex = /01 Oct - 31 Dec 2023/;
-    expect(screen.getByText(dateRangeRegex)).toBeInTheDocument();
+    // Initial state expectations
+    const today = moment().startOf('day');
+    const expectedStartDate = today.clone().subtract(7, 'days');
+    const expectedEndDate = today;
+
+    expect(
+      screen.getByText(
+        `${expectedStartDate.format('DD MMM')} - ${expectedEndDate.format('DD MMM YYYY')}`
+      )
+    ).toBeInTheDocument();
     expect(screen.getByText(exportCSVText)).toBeInTheDocument();
     expect(screen.getByText(initDateRange)).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('Last 7 Days'));
+    // Trigger the "Last 30 Days" mode
+    fireEvent.click(screen.getByText('Last 30 Days'));
 
-    await waitFor(() => {
-      expect(screen.getByText('7 Days')).toBeInTheDocument();
-      expect(screen.getByText('30 Days')).toBeInTheDocument();
-      expect(screen.getByText('90 Days')).toBeInTheDocument();
-      expect(screen.getByText('Custom')).toBeInTheDocument();
-    });
+    const expectedStartDate30DaysMode = today.clone().subtract(30, 'days');
+    const expectedEndDate30DaysMode = today;
+    expect(
+      screen.getByText(
+        `${expectedStartDate30DaysMode.format('DD MMM')} - ${expectedEndDate30DaysMode.format(
+          'DD MMM YYYY'
+        )}`
+      )
+    ).toBeInTheDocument();
 
-    fireEvent.click(screen.getByText('30 Days'));
+    // Trigger the "Last 90 Days" mode
+    fireEvent.click(screen.getByText('Last 90 Days'));
 
-    const expectedStartDate = moment().subtract(30, 'days').startOf('day').unix();
-    const expectedEndDate = moment().startOf('day').unix();
+    const expectedStartDate90DaysMode = today.clone().subtract(90, 'days');
+    const expectedEndDate90DaysMode = today;
+    expect(
+      screen.getByText(
+        `${expectedStartDate90DaysMode.format('DD MMM')} - ${expectedEndDate90DaysMode.format(
+          'DD MMM YYYY'
+        )}`
+      )
+    ).toBeInTheDocument();
 
-    expect(setStartDateMock).toHaveBeenCalledWith(expectedStartDate);
-    expect(setEndDateMock).toHaveBeenCalledWith(expectedEndDate);
     expect(screen.getByText(exportCSVText)).toBeInTheDocument();
     expect(screen.getByText(initDateRange)).toBeInTheDocument();
   });
