a69ee7ad965c17d429294821edcb20a3ae0d93d7
Anish Yadav <anish.yadav@siemens.com>
2024-01-03 09:09:08 +0530

    chore: test db

--- routes/bounty.go
+++ routes/bounty.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/go-chi/chi"
 	"github.com/stakwork/sphinx-tribes/auth"
+	"github.com/stakwork/sphinx-tribes/db"
 	"github.com/stakwork/sphinx-tribes/handlers"
 	"net/http"
 )
 
 func BountyRoutes() chi.Router {
 	r := chi.NewRouter()
-	bountyHandler := handlers.NewBountyHandler(http.DefaultClient)
+	bountyHandler := handlers.NewBountyHandler(http.DefaultClient, db.DB)
 	r.Group(func(r chi.Router) {
 		r.Get("/all", handlers.GetAllBounties)
 		r.Get("/id/{bountyId}", handlers.GetBountyById)
