ff9e2ababbe9d63b1855939dbc88cf624fb2fcc8
Anish Yadav <anish.yadav@siemens.com>
2024-01-04 17:32:50 +0530

    chore: use mockery to create db interfaces

--- handlers/bounty.go
+++ handlers/bounty.go
@@ -130,7 +130,7 @@ func GetPersonAssignedBounties(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func CreateOrEditBounty(w http.ResponseWriter, r *http.Request) {
+func (h *bountyHandler) CreateOrEditBounty(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	pubKeyFromAuth, _ := ctx.Value(auth.ContextKey).(string)
 
@@ -181,22 +181,22 @@ func CreateOrEditBounty(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if bounty.Show == false && bounty.ID != 0 {
-		db.DB.UpdateBountyBoolColumn(bounty, "show")
+		h.db.UpdateBountyBoolColumn(bounty, "show")
 	}
 
 	if bounty.Title != "" && bounty.Assignee == "" {
-		db.DB.UpdateBountyNullColumn(bounty, "assignee")
+		h.db.UpdateBountyNullColumn(bounty, "assignee")
 	}
 
 	if bounty.Title != "" && bounty.ID != 0 {
 		// get bounty from DB
-		dbBounty := db.DB.GetBounty(bounty.ID)
+		dbBounty := h.db.GetBounty(bounty.ID)
 
 		// trying to update
 		// check if bounty belongs to user
 		if pubKeyFromAuth != dbBounty.OwnerID {
 			if bounty.OrgUuid != "" {
-				hasBountyRoles := db.UserHasManageBountyRoles(pubKeyFromAuth, bounty.OrgUuid)
+				hasBountyRoles := h.db.UserHasManageBountyRoles(pubKeyFromAuth, bounty.OrgUuid)
 				if !hasBountyRoles {
 					msg := "You don't have a=the right permission ton update bounty"
 					fmt.Println(msg)
@@ -214,7 +214,7 @@ func CreateOrEditBounty(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	b, err := db.DB.CreateOrEditBounty(bounty)
+	b, err := h.db.CreateOrEditBounty(bounty)
 	if err != nil {
 		fmt.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -225,7 +225,7 @@ func CreateOrEditBounty(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(b)
 }
 
-func DeleteBounty(w http.ResponseWriter, r *http.Request) {
+func (h *bountyHandler) DeleteBounty(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	pubKeyFromAuth, _ := ctx.Value(auth.ContextKey).(string)
 
@@ -249,8 +249,13 @@ func DeleteBounty(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	b, _ := db.DB.DeleteBounty(pubkey, created)
-
+	b, err := h.db.DeleteBounty(pubkey, created)
+	if err != nil {
+		fmt.Println("failed to delete bounty", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode("failed to delete bounty")
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(b)
 }
