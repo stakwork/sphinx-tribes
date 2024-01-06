5f221f7eab94f5745348fffc0774cf768e09be7d
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-06 23:20:14 +0530

    fix

--- frontend/app/src/people/main/MainHeader.spec.tsx
+++ frontend/app/src/people/main/MainHeader.spec.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom';
 import Header from './Header';
 
+let openSpy;
+
+beforeEach(() => {
+  openSpy = jest.spyOn(window, 'open');
+});
+
+afterEach(() => {
+  openSpy.mockRestore();
+});
+
 describe('Header Component', () => {
-  test('Clicking on "Get Sphinx" button should call the clickHandler function', async () => {
-    const mockHistoryPush = jest.fn();
-    const mockUseHistory = jest.fn(() => ({ push: mockHistoryPush }));
-    const mockWindowOpen = jest.spyOn(window, 'open').mockImplementation(() => window);
-
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useHistory: mockUseHistory
-    }));
-
-    const { getByText } = render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>
-    );
-
+  test('Clicking on "Get Sphinx" button should open a new window with the given URL', () => {
+    const { getByText } = render(<Header />);
     const getSphinxButton = getByText('Get Sphinx');
     fireEvent.click(getSphinxButton);
-    expect(mockWindowOpen).toHaveBeenCalledWith('https://buy.sphinx.chat/', '_blank');
+
+    expect(openSpy).toHaveBeenCalledWith('https://buy.sphinx.chat/', '_blank');
   });
 });
