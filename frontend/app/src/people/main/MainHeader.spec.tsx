efd7cfef623fa7f2cb70f1125f03c8cd8b697f56
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-07 00:40:51 +0530

    fix

--- frontend/app/src/people/main/MainHeader.spec.tsx
+++ frontend/app/src/people/main/MainHeader.spec.tsx
@@ -3,16 +3,6 @@ import { render, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './Header';
 
-let openSpy;
-
-beforeEach(() => {
-  openSpy = jest.spyOn(window, 'open');
-});
-
-afterEach(() => {
-  openSpy.mockRestore();
-});
-
 describe('Header Component', () => {
   test('Clicking on "Get Sphinx" button should open a new window with the given URL', () => {
     const { getByText } = render(
@@ -22,7 +12,7 @@ describe('Header Component', () => {
     );
     const getSphinxButton = getByText('Get Sphinx');
     fireEvent.click(getSphinxButton);
-
-    expect(openSpy).toEqual('https://buy.sphinx.chat/');
+   
+    expect(window).toEqual('https://buy.sphinx.chat/');
   });
 });
