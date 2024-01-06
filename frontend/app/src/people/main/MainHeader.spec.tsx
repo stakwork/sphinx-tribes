26914266a087248bd4f0461c7ebcf542986a7140
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-06 22:27:05 +0530

    test fix attempt

--- frontend/app/src/people/main/MainHeader.spec.tsx
+++ frontend/app/src/people/main/MainHeader.spec.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Header from './Header';
 
 describe('Header Component', () => {
-  test('Clicking on "Get Sphinx" button should call the clickHandler function', () => {
+  test('Clicking on "Get Sphinx" button should call the clickHandler function', async () => {
     const mockHistoryPush = jest.fn();
     const mockUseHistory = jest.fn(() => ({ push: mockHistoryPush }));
+    const mockWindowOpen = jest.spyOn(window, 'open').mockImplementation(() => window);
 
     jest.mock('react-router-dom', () => ({
       ...jest.requireActual('react-router-dom'),
-      useHistory: mockUseHistory
+      useHistory: mockUseHistory,
     }));
 
     const { getByText } = render(<Header />);
 
     const getSphinxButton = getByText('Get Sphinx');
     fireEvent.click(getSphinxButton);
-    expect(mockHistoryPush).toHaveBeenCalledWith('/');
+
+    await waitFor(() => {
+      expect(mockHistoryPush).toHaveBeenCalledWith('/');
+      expect(mockWindowOpen).toHaveBeenCalledWith('https://buy.sphinx.chat/');
+    });
+
+    mockWindowOpen.mockRestore();
   });
 });
