41fe91f21f1294e6c7150ba47c978386feaa8ef9
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-08 22:55:30 +0530

    update

--- frontend/app/src/people/widgetViews/__tests__/HistoryModal.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/HistoryModal.spec.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { PaymentHistory } from 'store/main';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { act } from 'react-dom/test-utils';
 import HistoryModal from '../organization/HistoryModal';
 
 jest.mock('hooks/uiHooks', () => ({
@@ -21,11 +20,9 @@ jest.mock('store', () => ({
 }));
 
 jest.mock('moment', () => () => ({ format: jest.fn(() => '01/01/2022') }));
-
 jest.mock('../../../helpers', () => ({
   formatSat: jest.fn((amount: any) => `${amount} sats`)
 }));
-
 jest.mock('../../../public/static/link.svg', () => 'LinkIcon');
 jest.mock('../../../public/static/arrow-right.svg', () => 'ArrowRight');
 
@@ -46,51 +43,95 @@ describe('HistoryModal component', () => {
       receiver_pubkey: 'receiverPubkey',
       receiver_name: 'Receiver Name',
       bounty_id: 123
+    },
+    {
+      id: 2,
+      org_uuid: 'organization_uuid',
+      status: true,
+      updated: '2022-01-02T00:00:00.000Z',
+      payment_type: 'deposit',
+      created: '2022-01-02T00:00:00.000Z',
+      amount: 200,
+      sender_img: 'depositorImage',
+      sender_pubkey: 'depositorPubkey',
+      sender_name: 'Depositor Name',
+      receiver_img: '',
+      receiver_pubkey: '',
+      receiver_name: '',
+      bounty_id: 456
+    },
+    {
+      id: 3,
+      org_uuid: 'organization_uuid',
+      status: true,
+      updated: '2022-01-03T00:00:00.000Z',
+      payment_type: 'withdraw',
+      created: '2022-01-03T00:00:00.000Z',
+      amount: 300,
+      sender_img: 'depositorImage',
+      sender_pubkey: 'depositorPubkey',
+      sender_name: 'Depositor Name',
+      receiver_img: 'receiverImage',
+      receiver_pubkey: 'receiverPubkey',
+      receiver_name: 'Receiver Name',
+      bounty_id: 789
     }
   ];
-  it('renders with mock data', () => {
+
+  it('renders with mock data for all transaction types', () => {
     const { getByText } = render(
       <HistoryModal
         isOpen
         paymentsHistory={mockPaymentsHistory}
-        close={() => {
-          jest.fn();
-        }}
+        close={() => {jest.fn()}}
         url=""
       />
     );
 
+    // Check for modal title and filter labels
     expect(getByText('Payment History')).toBeInTheDocument();
-
-    mockPaymentsHistory.forEach((payment: any) => {
-      expect(getByText('01/01/2022')).toBeInTheDocument();
-      expect(getByText(`${payment.amount} sats`)).toBeInTheDocument();
-      expect(getByText(payment.sender_name)).toBeInTheDocument();
-    });
-
     expect(getByText('Payments')).toBeInTheDocument();
     expect(getByText('Deposit')).toBeInTheDocument();
     expect(getByText('Withdrawals')).toBeInTheDocument();
+
+    // Check for each transaction type
+    mockPaymentsHistory.forEach((payment:PaymentHistory) => {
+      expect(getByText(`${payment.amount} sats`)).toBeInTheDocument();
+      expect(getByText('01/01/2022')).toBeInTheDocument(); // Using the mocked date format
+      expect(getByText(payment.sender_name)).toBeInTheDocument();
+      if (payment.payment_type === 'payment') {
+        expect(getByText(payment.receiver_name)).toBeInTheDocument();
+      }
+    });
   });
 
-  it('filters payments based on checkboxes', () => {
-    const { getByText, getByLabelText } = render(
+  it('renders deposit transactions without a receiver', () => {
+    const { queryByText } = render(
       <HistoryModal
         isOpen
-        paymentsHistory={mockPaymentsHistory}
-        close={() => {
-          jest.fn();
-        }}
+        paymentsHistory={mockPaymentsHistory.filter((payment: PaymentHistory) => payment.payment_type === 'deposit')}
+        close={() => {jest.fn()}}
         url=""
       />
     );
 
-    act(() => {
-      fireEvent.click(getByLabelText('Payments'));
-    });
+    // Deposit should have a sender but no receiver
+    expect(queryByText('Depositor Name')).toBeInTheDocument();
+    expect(queryByText('Receiver Name')).toBeNull();
+  });
 
-    expect(getByText('Deposit')).toBeInTheDocument();
-    expect(getByText('Withdrawals')).toBeInTheDocument();
-    expect(getByText('Payments')).toBeInTheDocument();
+  it('renders payment transactions with both sender and receiver', () => {
+    const { getByText } = render(
+      <HistoryModal
+        isOpen
+        paymentsHistory={mockPaymentsHistory.filter((payment: PaymentHistory) => payment.payment_type === 'payment')}
+        close={() => {jest.fn()}}
+        url=""
+      />
+    );
+
+    // Payment should have both a sender and a receiver
+    expect(getByText('Sender Name')).toBeInTheDocument();
+    expect(getByText('Receiver Name')).toBeInTheDocument();
   });
 });
