6343951a80d25dcfd2d37c7d53642dd56ac619d6
Ali Raza <87068339+aliraza556@users.noreply.github.com>
2024-01-08 20:40:31 +0500

    fix(no-badge): Fixed Badge Display Next to Filter on Mobile View (#1238)
    * fix(no-badge): fixed badge display next to filter on mobile view
    
    * fix(no-badge): fixed badge display next to filter on mobile view

--- frontend/app/src/people/widgetViews/BountyHeader.tsx
+++ frontend/app/src/people/widgetViews/BountyHeader.tsx
@@ -177,6 +177,28 @@ const FilterCount = styled.div<styledProps>`
   }
 `;
 
+const MobileFilterCount = styled.div<styledProps>`
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  margin-left: -8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -5px;
+  background: ${(p: any) => p?.color && p.color.blue1};
+  .filterCountText {
+    font-family: 'Barlow';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 13px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: ${(p: any) => p.color && p.color.pureWhite};
+  }
+`;
+
 const EuiPopOverCheckboxLeft = styled.div<styledProps>`
   width: 147px;
   height: 312px;
@@ -614,6 +636,11 @@ const BountyHeader = ({
                 </PopOverRightBox>
               </div>
             </EuiPopover>
+            {filterCountNumber > 0 && (
+              <MobileFilterCount color={color}>
+                <EuiText className="filterCountText">{filterCountNumber}</EuiText>
+              </MobileFilterCount>
+            )}
           </LargeActionContainer>
           <ShortActionContainer>
             <PostBounty widget={selectedWidget} />
