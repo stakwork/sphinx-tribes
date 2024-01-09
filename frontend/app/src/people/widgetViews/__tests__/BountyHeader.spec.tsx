e0e6d386a20eda0016ca82996861973fcc7e10e3
aliraza556 <alirazaakhtar1122@gmail.com>
2024-01-09 21:59:53 +0500

    [Test][Bounty Header] Create unit tests from BountyHeader.tsx

--- frontend/app/src/people/widgetViews/__tests__/BountyHeader.spec.tsx
+++ frontend/app/src/people/widgetViews/__tests__/BountyHeader.spec.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import BountyHeader from '../BountyHeader';
@@ -6,28 +6,25 @@ import { BountyHeaderProps } from '../../interfaces';
 import * as hooks from '../../../hooks';
 
 const mockProps: BountyHeaderProps = {
-    selectedWidget: 'people',
-    scrollValue: false,
-    onChangeStatus: jest.fn(),
-    onChangeLanguage: jest.fn(),
-    checkboxIdToSelectedMap: {},
-    checkboxIdToSelectedMapLanguage: {}
+  selectedWidget: 'people',
+  scrollValue: false,
+  onChangeStatus: jest.fn(),
+  onChangeLanguage: jest.fn(),
+  checkboxIdToSelectedMap: {},
+  checkboxIdToSelectedMapLanguage: {}
 };
 
 jest.mock('../../../hooks', () => ({
-    useIsMobile: jest.fn(),
+  useIsMobile: jest.fn()
 }));
 
 describe('BountyHeader Component Tests', () => {
+  beforeEach(() => {
+    (hooks.useIsMobile as jest.Mock).mockReturnValue(true);
+  });
 
-    beforeEach(() => {
-        // Mock useIsMobile to return true before each test
-        (hooks.useIsMobile as jest.Mock).mockReturnValue(true);
-    });
-
-   
-    test('renders filters', () => {
-        render(<BountyHeader {...mockProps} />);
-        expect(screen.getByText(/Filter/i)).toBeInTheDocument();
-    });
+  test('renders filters', () => {
+    render(<BountyHeader {...mockProps} />);
+    expect(screen.getByText(/Filter/i)).toBeInTheDocument();
+  });
 });
