dfb3c92358064e24b2dcc3be0967fa37cb63bb76
aliraza556 <alirazaakhtar1122@gmail.com>
2024-01-17 10:27:27 +0500

    fix(testing): unit test v4

--- frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
+++ frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import { Router, Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { TicketModalPage } from '../../TicketModalPage.tsx';
 
 describe('<TicketModalPage />', () => {
-  it('should navigate to the correct URL when accessed directly and goBack is called', () => {
+  it('should navigate to the correct URL when accessed directly and goBack is called', async () => {
     const history = createMemoryHistory({ initialEntries: ['/bounty/1181'] });
 
+    // Mock referrer if necessary
     Object.defineProperty(document, 'referrer', {
       value: '',
       writable: true
     });
 
-    const { getByTestId } = render(
+    render(
       <Router history={history}>
         <Route path="/bounty/:bountyId">
           <TicketModalPage setConnectPerson={jest.fn()} />
@@ -21,7 +22,9 @@ describe('<TicketModalPage />', () => {
       </Router>
     );
 
-    const goBackButton = getByTestId('close-btn');
+    // Wait for the modal to be in the document
+    const goBackButton = await waitFor(() => screen.findByTestId('close-btn'));
+
     fireEvent.click(goBackButton);
 
     expect(history.location.pathname).toBe('/bounties');
