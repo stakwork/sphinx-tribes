e8bc3a505ae45aefe0fa8d99a4e5d03cd0f48584
aliraza556 <alirazaakhtar1122@gmail.com>
2024-01-17 13:07:17 +0500

    fix(testing): unit test v15

--- frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
+++ frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import { Router, Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { TicketModalPage } from '../../TicketModalPage'; // Adjust the import path as needed
+import { TicketModalPage } from '../../TicketModalPage.tsx';
 
-describe('TicketModalPage', () => {
-  it('redirects to the bounty home page on direct access and modal close', () => {
+describe('<TicketModalPage />', () => {
+  it('should navigate to the correct URL when accessed directly and goBack is called', async () => {
     const history = createMemoryHistory({ initialEntries: ['/bounty/1181'] });
-
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useHistory: () => history
-    }));
 
     const { getByTestId } = render(
       <Router history={history}>
@@ -21,11 +16,13 @@ describe('TicketModalPage', () => {
       </Router>
     );
 
-    // Simulate clicking the big close button in the modal
-    const bigCloseButton = getByTestId('close-btn');
+    let bigCloseButton;
+    await waitFor(() => {
+      bigCloseButton = getByTestId('close-btn');
+    });
+
     fireEvent.click(bigCloseButton);
 
-    // Check if the current URL is the bounty home page
     expect(history.location.pathname).toBe('/bounties');
   });
 });
