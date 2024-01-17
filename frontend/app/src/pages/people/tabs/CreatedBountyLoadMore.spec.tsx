fd7ae85e4048c0566a0b86c9022e7a065ef1e208
Vividh Pandey <vividh.pandey.13@gmail.com>
2024-01-17 16:24:00 +0530

    prettier
    Signed-off-by: Vividh Pandey <vividh.pandey.13@gmail.com>

--- frontend/app/src/pages/people/tabs/CreatedBountyLoadMore.spec.tsx
+++ frontend/app/src/pages/people/tabs/CreatedBountyLoadMore.spec.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import mockBounties from '../../../bounties/__mock__/mockBounties.data';
 import '@testing-library/jest-dom/extend-expect';
 import { Wanted } from './Wanted';
 
 // eslint-disable-next-line @typescript-eslint/typedef
 const createdMockBounties = Array.from({ length: 25 }, (_, index) => ({
-  ...(mockBounties[0] || {}), 
+  ...(mockBounties[0] || {}),
   bounty: {
-    ...(mockBounties[0]?.bounty || {}), 
+    ...(mockBounties[0]?.bounty || {}),
     id: mockBounties[0]?.bounty?.id + index + 1
   }
 }));
@@ -19,8 +20,14 @@ jest.mock('../../../bounties/__mock__/mockBounties.data', () => ({
 
 describe('Wanted component', () => {
   it('displays "Load More" button when scrolling down', async () => {
-    const { getByText } = render(<Wanted />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Wanted />
+      </MemoryRouter>
+    );
+
     fireEvent.scroll(window, { target: { scrollY: 1000 } });
+
     await waitFor(() => {
       expect(getByText('Load More')).toBeInTheDocument();
     });
