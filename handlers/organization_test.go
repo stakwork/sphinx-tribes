b549f7bbe01660cf4e6a370aab46b5c02b892a19
Gourav Kadu <gouravmpk@gmail.com>
2024-01-17 18:10:26 +0530

    Update organization_test.go

--- handlers/organization_test.go
+++ handlers/organization_test.go
@@ -3,18 +3,14 @@ package handlers
 import (
 	"bytes"
 	"context"
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-
-	"testing"
-
 	"github.com/stakwork/sphinx-tribes/auth"
 	"github.com/stakwork/sphinx-tribes/db"
 	mocks "github.com/stakwork/sphinx-tribes/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestUnitCreateOrEditOrganization(t *testing.T) {
@@ -41,7 +37,7 @@ func TestUnitCreateOrEditOrganization(t *testing.T) {
 		rr := httptest.NewRecorder()
 		handler := http.HandlerFunc(oHandler.CreateOrEditOrganization)
 
-		invalidJson := []byte(`{"name": "value"}`)
+		invalidJson := []byte(`{"key": "value"}`)
 		req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, "/", bytes.NewReader(invalidJson))
 		if err != nil {
 			t.Fatal(err)
@@ -92,10 +88,8 @@ func TestUnitCreateOrEditOrganization(t *testing.T) {
 			return org.Name == "TestOrganization" && org.Uuid != "" && org.Updated != nil && org.Created != nil
 		})).Return(db.Organization{}, nil).Once()
 
-
-		validJson :=  []byte(`{"name": "TestOrganization", "owner_pubkey": "test-key" ,"description": "Test"}`)
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/", bytes.NewReader(validJson))
-
+		invalidJson := []byte(`{"name": "TestOrganization", "owner_pubkey": "test-key"}`)
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/", bytes.NewReader(invalidJson))
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -103,32 +97,5 @@ func TestUnitCreateOrEditOrganization(t *testing.T) {
 		handler.ServeHTTP(rr, req)
 
 		assert.Equal(t, http.StatusOK, rr.Code)
-	})
-	t.Run("should return error if org description is empty or too long", func(t *testing.T) {
-		tests := []struct {
-			name        string
-			description string
-			wantStatus  int
-		}{
-			{"empty description", "", http.StatusBadRequest},
-			{"long description", strings.Repeat("a", 121), http.StatusBadRequest},
-		}
-
-		for _, tc := range tests {
-			t.Run(tc.description, func(t *testing.T) {
-				rr := httptest.NewRecorder()
-				handler := http.HandlerFunc(oHandler.CreateOrEditOrganization)
-				invalidJson := []byte(fmt.Sprintf(`{"name": "TestOrganization", "owner_pubkey": "test-key", "description": "%s"}`, tc.description))
-
-				req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/", bytes.NewReader(invalidJson))
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				handler.ServeHTTP(rr, req)
-
-				assert.Equal(t, tc.wantStatus, rr.Code)
-			})
-		}
 	})
 }
