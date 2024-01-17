8015f1cddaabe55de083605d445a57597790c0c9
hritikmunde <hritik16munde@gmail.com>
2024-01-17 19:12:12 +0530

    prettier fix

--- frontend/app/src/__test__/__mockData__/organization.ts
+++ frontend/app/src/__test__/__mockData__/organization.ts
@@ -1,30 +1,28 @@
-import { Organization } from "store/main";
+import { Organization } from 'store/main';
 
 export const organizations: Organization[] = [
   {
     bounty_count: 0,
-    created: "2024-01-03T20:34:09.585609Z",
+    created: '2024-01-03T20:34:09.585609Z',
     deleted: false,
-    id: "51",
-    img: "",
-    name: "TEST_NEW",
-    owner_pubkey:
-      "03cbb9c01cdcf91a3ac3b543a556fbec9c4c3c2a6ed753e19f2706012a26367ae3",
+    id: '51',
+    img: '',
+    name: 'TEST_NEW',
+    owner_pubkey: '03cbb9c01cdcf91a3ac3b543a556fbec9c4c3c2a6ed753e19f2706012a26367ae3',
     show: false,
-    updated: "2024-01-03T20:34:09.585609Z",
-    uuid: "cmas9gatu2rvqiev4ur0",
+    updated: '2024-01-03T20:34:09.585609Z',
+    uuid: 'cmas9gatu2rvqiev4ur0'
   },
   {
     bounty_count: 0,
-    created: "2024-01-03T20:34:09.585609Z",
+    created: '2024-01-03T20:34:09.585609Z',
     deleted: false,
-    id: "52",
-    img: "",
-    name: "TEST_SECOND",
-    owner_pubkey:
-      "03cbb9c01cdcf91a3ac3b543a556fbec9c4c3c2a6ed753e19f2706012a26367ae3",
+    id: '52',
+    img: '',
+    name: 'TEST_SECOND',
+    owner_pubkey: '03cbb9c01cdcf91a3ac3b543a556fbec9c4c3c2a6ed753e19f2706012a26367ae3',
     show: false,
-    updated: "2024-01-03T20:34:09.585609Z",
-    uuid: "cmas9gatu2rvqiev4ur0",
-  },
+    updated: '2024-01-03T20:34:09.585609Z',
+    uuid: 'cmas9gatu2rvqiev4ur0'
+  }
 ];
