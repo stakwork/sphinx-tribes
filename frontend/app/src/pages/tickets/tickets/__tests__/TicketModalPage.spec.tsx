b83baef7da5cde7e193ec156f8b7ee51cb4cdcf9
aliraza556 <alirazaakhtar1122@gmail.com>
2024-01-17 11:11:07 +0500

    fix(testing): unit test v8

--- frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
+++ frontend/app/src/pages/tickets/tickets/__tests__/TicketModalPage.spec.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { Router, Route } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { TicketModalPage } from '../../TicketModalPage';
 
-// Mock useHistory
-const mockHistoryPush = jest.fn();
-const mockHistoryGoBack = jest.fn();
-
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useHistory: () => ({
-    push: mockHistoryPush,
-    goBack: mockHistoryGoBack
+    push: jest.fn(),
+    goBack: jest.fn()
   })
 }));
 
 describe('<TicketModalPage />', () => {
-  beforeEach(() => {
-    // Clear all mock function calls before each test
-    mockHistoryPush.mockClear();
-    mockHistoryGoBack.mockClear();
-  });
-
   it('should navigate to the correct URL when accessed directly and goBack is called', async () => {
-    // Create a memory history for your test
     const history = createMemoryHistory({ initialEntries: ['/bounty/1181'] });
+    Object.defineProperty(window.document, 'referrer', {
+      value: '',
+      configurable: true
+    });
 
-    // Render the component inside a Router context
     render(
       <Router history={history}>
         <Route path="/bounty/:bountyId">
@@ -36,9 +28,9 @@ describe('<TicketModalPage />', () => {
       </Router>
     );
 
-    await waitFor(() => {
-      expect(mockHistoryPush).toHaveBeenCalledWith('/bounties');
-      // or expect(mockHistoryGoBack).toHaveBeenCalled();
+    await act(async () => {
+      expect(history.push).toHaveBeenCalledWith('/bounties');
+      // or expect(history.goBack).toHaveBeenCalled();
     });
   });
 });
