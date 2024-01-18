97c0495c01bcd744d48be8e504e4ef3d097f27fe
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-18 23:03:28 +0530

    fix

--- routes/person.go
+++ routes/person.go
@@ -1,16 +1,20 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/stakwork/sphinx-tribes/auth"
+	"github.com/stakwork/sphinx-tribes/db"
 	"github.com/stakwork/sphinx-tribes/handlers"
 )
 
 func PersonRoutes() chi.Router {
 	r := chi.NewRouter()
+	personHandler := handlers.NewPersonyHandler(http.DefaultClient, db.DB)
 	r.Group(func(r chi.Router) {
 		r.Get("/{pubkey}", handlers.GetPersonByPubkey)
-		r.Get("/id/{id}", handlers.GetPersonById)
+		r.Get("/id/{id}", personHandler.GetPersonById)
 		r.Get("/uuid/{uuid}", handlers.GetPersonByUuid)
 		r.Get("/uuid/{uuid}/assets", handlers.GetPersonAssetsByUuid)
 		r.Get("/githubname/{github}", handlers.GetPersonByGithubName)
