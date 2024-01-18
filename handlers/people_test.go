f57a2d38b562a159f7233d543e403056942cfce8
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-18 19:06:47 +0530

    feat(#1388): test people.go handlers GetPersonById & DeletePerson

---
+++ handlers/people_test.go
@@ -0,0 +1,78 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPersonByIdWithInvalidAuthToken(t *testing.T) {
+	req, err := http.NewRequest("GET", "/person/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "invalid_token")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetPersonById)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+}
+
+func TestGetPersonById(t *testing.T) {
+	req, err := http.NewRequest("GET", "/person/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetPersonById)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `{"id":1,"other":"fields"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestDeletePersonSuccess(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/person/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "valid_token")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeletePerson)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `true`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestDeletePersonFailure(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/person/1", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization", "invalid_token")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(DeletePerson)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+}
