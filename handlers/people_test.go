8f2a20e0aef93cf44040d25c8f8e4f661ee547f8
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-18 20:38:20 +0530

    test rewriteclear

--- handlers/people_test.go
+++ handlers/people_test.go
@@ -6,47 +6,24 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/go-chi/chi"
 	"github.com/stakwork/sphinx-tribes/auth"
-	"github.com/stakwork/sphinx-tribes/db"
-	dbMocks "github.com/stakwork/sphinx-tribes/mocks"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestDeletePerson(t *testing.T) {
-
-	mockDb := dbMocks.NewDatabase(t)
-
-	mockUser := &db.Person{
-		ID:          1,
-		OwnerPubKey: "authorized-key",
+	rr := httptest.NewRecorder()
+	req, err := http.NewRequest("DELETE", "/person/1", nil)
+	if err != nil {
+		t.Fatal(err)
 	}
-	mockDb.On("GetPerson", uint(1)).Return(mockUser, nil)
-
-	r := chi.NewRouter()
-	r.Delete("/{id}", DeletePerson)
-
-	t.Run("unauthorized deletion due to pubkey mismatch", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodDelete, "/1", nil)
-		rr := httptest.NewRecorder()
-		ctx := context.WithValue(req.Context(), "db", mockDb)
-		req = req.WithContext(ctx)
-		ctx = context.WithValue(ctx, auth.ContextKey, "unauthorized-key")
-		req = req.WithContext(ctx)
-		r.ServeHTTP(rr, req)
-		assert.Equal(t, http.StatusUnauthorized, rr.Code)
-	})
-
-	t.Run("authorized deletion", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodDelete, "/1", nil)
-		rr := httptest.NewRecorder()
-		ctx := context.WithValue(req.Context(), "db", mockDb)
-		req = req.WithContext(ctx)
-		ctx = context.WithValue(ctx, auth.ContextKey, "authorized-key")
-		req = req.WithContext(ctx)
-		r.ServeHTTP(rr, req)
-		assert.Equal(t, http.StatusOK, rr.Code)
-	})
-
-	mockDb.AssertExpectations(t)
+	ctx := context.WithValue(req.Context(), auth.ContextKey, "pubKey")
+	req = req.WithContext(ctx)
+	DeletePerson(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	expected := `true`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
 }
+
