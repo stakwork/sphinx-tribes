1249584e1e01ef0a4b8964b3c531dd73c2d8d6d6
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-18 20:03:11 +0530

    Test Rewrite

--- handlers/people_test.go
+++ handlers/people_test.go
@@ -1,45 +1,38 @@
 package handlers
 
 import (
-	"context"
-	"net/http"
-	"net/http/httptest"
-	"testing"
-
-	"github.com/stakwork/sphinx-tribes/auth"
-	"github.com/stakwork/sphinx-tribes/db"
-	"github.com/go-chi/chi"
-	dbMocks "github.com/stakwork/sphinx-tribes/mocks"
-	"github.com/stretchr/testify/assert"
+    "context"
+    "net/http"
+    "net/http/httptest"
+    "testing"
+    "github.com/go-chi/chi"
+    "github.com/stretchr/testify/assert"
+    "github.com/stakwork/sphinx-tribes/db"
+    dbMocks "github.com/stakwork/sphinx-tribes/mocks"
+    "github.com/stakwork/sphinx-tribes/auth"
 )
 
 func TestDeletePerson(t *testing.T) {
+
     mockDb := dbMocks.NewDatabase(t)
-    ctx := context.WithValue(context.Background(), auth.ContextKey, "test-key")
+    
+    mockUser := &db.Person{
+        ID: 1,
+        OwnerPubKey: "authorized-key",
+    }
+    mockDb.On("GetPerson", uint(1)).Return(mockUser, nil)
+    r := chi.NewRouter()
+    r.Delete("/{id}", DeletePerson)
 
     t.Run("unauthorized deletion due to pubkey mismatch", func(t *testing.T) {
+        req := httptest.NewRequest(http.MethodDelete, "/1", nil)
         rr := httptest.NewRecorder()
-
-        // Mocking the database response
-        mockUser := db.Person{
-            ID: 1,
-            OwnerPubKey: "authorized-key",
-        }
-        mockDb.On("GetPerson", uint(1)).Return(mockUser)
-
-        // Creating the request with the appropriate context
-        rctx := chi.NewRouteContext()
-        rctx.URLParams.Add("id", "1")
-        req, err := http.NewRequestWithContext(context.WithValue(ctx, chi.RouteCtxKey, rctx), http.MethodDelete, "/", nil)
-        if err != nil {
-            t.Fatal(err)
-        }
-
-        // Calling the handler function
-        DeletePerson(rr, req)
-
-        // Asserting the response
+        ctx := context.WithValue(req.Context(), "db", mockDb)
+        req = req.WithContext(ctx)
+        ctx = context.WithValue(ctx, auth.ContextKey, "unauthorized-key")
+        req = req.WithContext(ctx)
+        r.ServeHTTP(rr, req)
         assert.Equal(t, http.StatusUnauthorized, rr.Code)
     })
+    mockDb.AssertExpectations(t)
 }
-
