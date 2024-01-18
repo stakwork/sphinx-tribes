d15162f71812a0b0741b1ab7e2cc020a9cc0071d
Kumar Nayan <nayanr489@gmail.com>
2024-01-18 20:21:00 +0530

    fix test case

--- handlers/metrics_test.go
+++ handlers/metrics_test.go
@@ -128,7 +128,7 @@ func TestBountyMetrics(t *testing.T) {
 
 		dateRange := db.PaymentDateRange{
 			StartDate: "1111",
-			EndDate:   "1212",
+			EndDate:   "2222",
 		}
 		body, _ := json.Marshal(dateRange)
 		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/bounty_stats", bytes.NewReader(body))
@@ -148,8 +148,8 @@ func TestBountyMetrics(t *testing.T) {
 		handler := http.HandlerFunc(mh.BountyMetrics)
 
 		dateRange := db.PaymentDateRange{
-			StartDate: "0112",
-			EndDate:   "0212",
+			StartDate: "1111",
+			EndDate:   "2222",
 		}
 		body, _ := json.Marshal(dateRange)
 		req, err := http.NewRequestWithContext(ctx, http.MethodPost, "/bounty_count", bytes.NewReader(body))
