cb30ffb9722c6c1671cb51bc74e033477fdf43ef
Anish Yadav <anish.yadav@siemens.com>
2024-01-18 23:21:26 +0530

    chore: add tests for people handler

--- routes/person.go
+++ routes/person.go
@@ -3,13 +3,15 @@ package routes
 import (
 	"github.com/go-chi/chi"
 	"github.com/stakwork/sphinx-tribes/auth"
+	"github.com/stakwork/sphinx-tribes/db"
 	"github.com/stakwork/sphinx-tribes/handlers"
 )
 
 func PersonRoutes() chi.Router {
 	r := chi.NewRouter()
+	peopleHandler := handlers.NewPeopleHandler(db.DB)
 	r.Group(func(r chi.Router) {
-		r.Get("/{pubkey}", handlers.GetPersonByPubkey)
+		r.Get("/{pubkey}", peopleHandler.GetPersonByPubkey)
 		r.Get("/id/{id}", handlers.GetPersonById)
 		r.Get("/uuid/{uuid}", handlers.GetPersonByUuid)
 		r.Get("/uuid/{uuid}/assets", handlers.GetPersonAssetsByUuid)
@@ -18,7 +20,7 @@ func PersonRoutes() chi.Router {
 	r.Group(func(r chi.Router) {
 		r.Use(auth.PubKeyContext)
 
-		r.Post("/", handlers.CreateOrEditPerson)
+		r.Post("/", peopleHandler.CreateOrEditPerson)
 		r.Delete("/{id}", handlers.DeletePerson)
 	})
 	return r
