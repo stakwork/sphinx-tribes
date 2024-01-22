23168871631912f37d30f7b5ca4154f13224aab2
Tushar Vyas <89837102+Vayras@users.noreply.github.com>
2024-01-22 14:27:08 +0530

    Test fix

--- frontend/app/src/components/common/Modal.spec.tsx
+++ frontend/app/src/components/common/Modal.spec.tsx
@@ -3,24 +3,10 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import Modal from './Modal';
 
 describe('<Modal /> Arrow Buttons', () => {
-  test('renders the previous arrow when prevArrowNew is provided', () => {
-    const prevArrowFunction = jest.fn();
-    render(<Modal prevArrowNew={prevArrowFunction} visible={true} />);
-    const prevArrow = screen.getByTestId('prev-arrow');
-    expect(prevArrow).toBeInTheDocument();
-  });
-
-  test('renders the next arrow when nextArrowNew is provided', () => {
-    const nextArrowFunction = jest.fn();
-    render(<Modal nextArrowNew={nextArrowFunction} visible={true} />);
-    const nextArrow = screen.getByTestId('next-arrow');
-    expect(nextArrow).toBeInTheDocument();
-  });
-
   test('calls prevArrowNew function when previous arrow is clicked', () => {
     const prevArrowFunction = jest.fn();
     render(<Modal prevArrowNew={prevArrowFunction} visible={true} />);
-    const prevArrow = screen.getByTestId('prev-arrow');
+    const prevArrow = screen.getByText('chevron_left');
     fireEvent.click(prevArrow);
     expect(prevArrowFunction).toHaveBeenCalled();
   });
@@ -28,7 +14,7 @@ describe('<Modal /> Arrow Buttons', () => {
   test('calls nextArrowNew function when next arrow is clicked', () => {
     const nextArrowFunction = jest.fn();
     render(<Modal nextArrowNew={nextArrowFunction} visible={true} />);
-    const nextArrow = screen.getByTestId('next-arrow');
+    const nextArrow = screen.getByText('chevron_right');
     fireEvent.click(nextArrow);
     expect(nextArrowFunction).toHaveBeenCalled();
   });
