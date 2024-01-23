1e52644b02f61ae77e66e22cec704df923333964
gouravmpk <gouravmpk@gmail.com>
2024-01-24 01:19:16 +0530

    Msg Change

--- frontend/app/src/components/form/bounty/index.tsx
+++ frontend/app/src/components/form/bounty/index.tsx
@@ -225,7 +225,7 @@ function Form(props: FormProps) {
         );
         const onClickHandler = () => {
           if (!isOnline) {
-            alert('No internet connection. Please try again later.');
+            alert('Bounty creation failed. Please try again.');
             return;
           }
 
