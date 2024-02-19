84e58c368cb42d8fd5d9fc1575c76d2da40d0dab
Abdul Wahab <abdulwahab3181@gmail.com>
2024-02-19 21:58:25 +0500

    Added UTs for GetPeopleBySearch, GetListedPeople, GetPersonByUuid

--- routes/people.go
+++ routes/people.go
@@ -2,14 +2,16 @@ package routes
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/stakwork/sphinx-tribes/db"
 	"github.com/stakwork/sphinx-tribes/handlers"
 )
 
 func PeopleRoutes() chi.Router {
 	r := chi.NewRouter()
+	peopleHandler := handlers.NewPeopleHandler(db.DB)
 	r.Group(func(r chi.Router) {
-		r.Get("/", handlers.GetListedPeople)
-		r.Get("/search", handlers.GetPeopleBySearch)
+		r.Get("/", peopleHandler.GetListedPeople)
+		r.Get("/search", peopleHandler.GetPeopleBySearch)
 		r.Get("/posts", handlers.GetListedPosts)
 		r.Get("/wanteds/assigned/{uuid}", handlers.GetPersonAssignedBounties)
 		r.Get("/wanteds/created/{uuid}", handlers.GetPersonCreatedBounties)
