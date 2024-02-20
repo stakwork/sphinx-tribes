b5c346ad48c3b415466ac223f99364248b6efb4a
gouravmpk <gouravmpk@gmail.com>
2024-02-21 04:36:51 +0530

    routes

--- routes/bounty.go
+++ routes/bounty.go
@@ -15,8 +15,8 @@ func BountyRoutes() chi.Router {
 	r.Group(func(r chi.Router) {
 		r.Get("/all", bountyHandler.GetAllBounties)
 
-		r.Get("/id/{bountyId}", handlers.GetBountyById)
-		r.Get("/index/{bountyId}", handlers.GetBountyIndexById)
+		r.Get("/id/{bountyId}", bountyHandler.GetBountyById)
+		r.Get("/index/{bountyId}", bountyHandler.GetBountyIndexById)
 		r.Get("/next/{created}", bountyHandler.GetNextBountyByCreated)
 		r.Get("/previous/{created}", bountyHandler.GetPreviousBountyByCreated)
 		r.Get("/org/next/{uuid}/{created}", bountyHandler.GetOrganizationNextBountyByCreated)
