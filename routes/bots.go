2e0d453b41a6b2604905b121aca30ee299b91ba1
MahtabBukhari <mahtabali50447@gmail.com>
2024-02-29 21:43:03 +0500

    tests for bots dot go file

--- routes/bots.go
+++ routes/bots.go
@@ -2,15 +2,17 @@ package routes
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/stakwork/sphinx-tribes/db"
 	"github.com/stakwork/sphinx-tribes/handlers"
 )
 
 func BotsRoutes() chi.Router {
 	r := chi.NewRouter()
+	botHandler := handlers.NewBotHandler(db.DB)
 	r.Group(func(r chi.Router) {
 		r.Post("/", handlers.CreateOrEditBot)
 		r.Get("/", handlers.GetListedBots)
-		r.Get("/owner/{pubkey}", handlers.GetBotsByOwner)
+		r.Get("/owner/{pubkey}", botHandler.GetBotsByOwner)
 		r.Get("/{uuid}", handlers.GetBot)
 	})
 	return r
