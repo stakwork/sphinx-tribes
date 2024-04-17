68e9f1099bca1fa2744b5cff1e659908e1679127
elraphty <elraphtyofficial@gmail.com>
2024-04-17 13:35:45 +0100

    added count to budget and added workspace/next endpoint

--- routes/bounty.go
+++ routes/bounty.go
@@ -21,6 +21,8 @@ func BountyRoutes() chi.Router {
 		r.Get("/previous/{created}", bountyHandler.GetPreviousBountyByCreated)
 		r.Get("/org/next/{uuid}/{created}", bountyHandler.GetWorkspaceNextBountyByCreated)
 		r.Get("/org/previous/{uuid}/{created}", bountyHandler.GetWorkspacePreviousBountyByCreated)
+		r.Get("/workspace/next/{uuid}/{created}", bountyHandler.GetWorkspaceNextBountyByCreated)
+		r.Get("/workspace/previous/{uuid}/{created}", bountyHandler.GetWorkspacePreviousBountyByCreated)
 
 		r.Get("/created/{created}", bountyHandler.GetBountyByCreated)
 		r.Get("/count/{personKey}/{tabType}", handlers.GetUserBountyCount)
