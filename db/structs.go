89f068667a9226d1d7ceda0231e28be3ff37b85b
Abdul Wahab <abdulwahab3181@gmail.com>
2024-04-26 10:15:13 +0500

    Removed Mission, Tactics and SchematicUrl

--- db/structs.go
+++ db/structs.go
@@ -509,9 +509,6 @@ type Workspace struct {
 	Website      string     `json:"website" validate:"omitempty,uri"`
 	Github       string     `json:"github" validate:"omitempty,uri"`
 	Description  string     `json:"description" validate:"omitempty,lte=120"`
-	Mission      *string    `json:"mission,omitempty"`
-    Tactics      *string    `json:"tactics,omitempty"`
-    SchematicUrl *string    `json:"schematic_url,omitempty"`
 }
 
 type WorkspaceShort struct {
