94078d2c024e76bee239c5c6e8a1e8ecbacd87fe
l <fvalentiner@entrepidus.com>
2024-04-30 08:53:49 -0400

    Added cypress tests and objects

---
+++ cypress/e2e/07_add_bounty_to_phase.cy.ts
@@ -0,0 +1,20 @@
+import { User, HostName, Workspaces, Bounties } from '../support/objects/objects';
+
+
+describe('Create Bounty', () => {
+    it('passes', () => {
+        cy.upsertlogin(User).then(value => {
+            for(let i = 0; i <= 2; i++) {
+                cy.request({
+                    method: 'POST',
+                    url: `${HostName}/gobounties/`,
+                    headers: { 'x-jwt': `${value}` },
+                    body: Bounties[i]
+                })//.its('body').should('have.property', 'id', Workspaces[i].name.trim())
+                .then( value => {
+                    console.log(value);
+                });
+            }
+        })
+    })
+})
