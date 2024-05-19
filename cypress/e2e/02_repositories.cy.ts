cf7b75863ac9c12973c998270dbe5a3a79491c60
elraphty <elraphtyofficial@gmail.com>
2024-05-19 16:39:17 +0100

    fixed merge conflicts

--- cypress/e2e/02_repositories.cy.ts
+++ cypress/e2e/02_repositories.cy.ts
@@ -5,7 +5,7 @@ import { User, HostName, Workspaces, Repositories } from '../support/objects/obj
 describe('Create Repositories for Workspace', () => {
     it('passes', () => {
         cy.upsertlogin(User).then(value => {
-            for(let i = 0; i <= 1; i++) {
+            for (let i = 0; i <= 1; i++) {
                 cy.request({
                     method: 'POST',
                     url: `${HostName}/workspaces/repositories`,
@@ -14,7 +14,7 @@ describe('Create Repositories for Workspace', () => {
                 }).its('body').then(body => {
                     expect(body).to.have.property('name').and.equal(Repositories[i].name.trim());
                     expect(body).to.have.property('url').and.equal(Repositories[i].url.trim());
-                });                
+                });
             }
         })
     })
@@ -23,7 +23,7 @@ describe('Create Repositories for Workspace', () => {
 describe('Modify Repository name for Workspace', () => {
     it('passes', () => {
         cy.upsertlogin(User).then(value => {
-            for(let i = 0; i <= 1; i++) {
+            for (let i = 0; i <= 1; i++) {
                 cy.request({
                     method: 'POST',
                     url: `${HostName}/workspaces/repositories`,
@@ -44,7 +44,7 @@ describe('Modify Repository name for Workspace', () => {
 describe('Modify Repository url for Workspace', () => {
     it('passes', () => {
         cy.upsertlogin(User).then(value => {
-            for(let i = 0; i <= 1; i++) {
+            for (let i = 0; i <= 1; i++) {
                 cy.request({
                     method: 'POST',
                     url: `${HostName}/workspaces/repositories`,
@@ -62,16 +62,54 @@ describe('Modify Repository url for Workspace', () => {
     })
 })
 
+describe('Modify Repository name for Workspace', () => {
+    it('passes', () => {
+        cy.upsertlogin(User).then(value => {
+            for (let i = 0; i <= 1; i++) {
+                cy.request({
+                    method: 'POST',
+                    url: `${HostName}/workspaces/repositories`,
+                    headers: { 'x-jwt': `${value}` },
+                    body: {
+                        uuid: Repositories[i].uuid,
+                        name: Repositories[i].name.trim() + "_addText"
+                    }
+                }).its('body').should('have.property', 'name', Repositories[i].name.trim() + "_addText")
+                    .its('body').should('have.property', 'url', Repositories[i].url.trim());
+            }
+        })
+    })
+})
+
+describe('Modify Repository url for Workspace', () => {
+    it('passes', () => {
+        cy.upsertlogin(User).then(value => {
+            for (let i = 0; i <= 1; i++) {
+                cy.request({
+                    method: 'POST',
+                    url: `${HostName}/workspaces/repositories`,
+                    headers: { 'x-jwt': `${value}` },
+                    body: {
+                        uuid: Repositories[i].uuid,
+                        url: Repositories[i].name.trim() + "_addText"
+                    }
+                }).its('body').should('have.property', 'name', Repositories[i].name.trim() + "_addText")
+                    .its('body').should('have.property', 'url', Repositories[i].url.trim() + "_addText");
+            }
+        })
+    })
+})
+
 
 describe('Check Repositories Values', () => {
     it('passes', () => {
         cy.upsertlogin(User).then(value => {
-            for(let i = 0; i <= 1; i++) {
+            for (let i = 0; i <= 1; i++) {
                 cy.request({
                     method: 'GET',
                     url: `${HostName}/workspaces/repositories/` + Repositories[i].workspace_uuid,
-                    headers: { 'x-jwt': `${ value }` },
-                    body: {} 
+                    headers: { 'x-jwt': `${value}` },
+                    body: {}
                 }).then((resp) => {
                     expect(resp.status).to.eq(200)
                     expect(resp.body[i]).to.have.property('name', Repositories[i].name.trim() + "_addText")
@@ -85,15 +123,16 @@ describe('Check Repositories Values', () => {
 describe('Get repository by uuid', () => {
     it('passes', () => {
         cy.upsertlogin(User).then(value => {
-            for(let i = 0; i <= 1; i++) {
+            for (let i = 0; i <= 2; i++) {
                 cy.request({
                     method: 'GET',
                     url: `${HostName}/workspaces/${Repositories[i].workspace_uuid}/repository/${Repositories[i].uuid}`,
-                    headers: { 'x-jwt': `${ value }` }
+                    headers: { 'x-jwt': `${value}` },
+                    body: {}
                 }).then((resp) => {
                     expect(resp.status).to.eq(200)
-                    expect(resp.body).to.have.property('name', Repositories[i].name.trim() + "_addText")
-                    expect(resp.body).to.have.property('url', Repositories[i].url.trim() + "_addText")
+                    expect(resp.body[i]).to.have.property('name', Repositories[i].name.trim() + "_addText")
+                    expect(resp.body[i]).to.have.property('url', Repositories[i].url.trim() + "_addText")
                 })
             }
         })
@@ -106,8 +145,8 @@ describe('Delete repository by uuid', () => {
             cy.request({
                 method: 'DELETE',
                 url: `${HostName}/workspaces/${Repositories[0].workspace_uuid}/repository/${Repositories[0].uuid}`,
-                headers: { 'x-jwt': `${ value }` },
-                body: {} 
+                headers: { 'x-jwt': `${value}` },
+                body: {}
             }).then((resp) => {
                 expect(resp.status).to.eq(200)
             })
@@ -121,9 +160,8 @@ describe('Check delete by uuid', () => {
             cy.request({
                 method: 'GET',
                 url: `${HostName}/workspaces/${Repositories[0].workspace_uuid}/repository/${Repositories[0].uuid}`,
-                headers: { 'x-jwt': `${ value }` },
-                body: {},
-                failOnStatusCode: false
+                headers: { 'x-jwt': `${value}` },
+                body: {}
             }).then((resp) => {
                 expect(resp.status).to.eq(404);
             })
