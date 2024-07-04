c754c95e78dfbba9298bba12cf6cec1589627c4e
MahtabBukhari <mahtabali50447@gmail.com>
2024-07-04 20:57:06 +0500

    Write Test For DeleteStory

--- handlers/features.go
+++ handlers/features.go
@@ -343,6 +343,14 @@ func (oh *featureHandler) GetStoryByUuid(w http.ResponseWriter, r *http.Request)
 	json.NewEncoder(w).Encode(story)
 }
 func (oh *featureHandler) DeleteStory(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	pubKeyFromAuth, _ := ctx.Value(auth.ContextKey).(string)
+	if pubKeyFromAuth == "" {
+		fmt.Println("no pubkey from auth")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	featureUuid := chi.URLParam(r, "feature_uuid")
 	storyUuid := chi.URLParam(r, "story_uuid")
 
