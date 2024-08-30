dece42a6713d80982969546a25b55ad6c5e2f576
aliraza556 <alirazaakhtar1122@gmail.com>
2024-08-30 18:45:36 +0500

    revert changes

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:run', () => {
-        console.log('V2_BOT_URL:', process.env.V2_BOT_URL);
-        console.log('V2_BOT_TOKEN:', process.env.V2_BOT_TOKEN);
-      });
-
-      on('uncaught:exception', (err, runnable) => {
-        // returning false here prevents Cypress from
-        // failing the test
-        return false;
-      });
+      // implement node event listeners here
     },
   },
 });
