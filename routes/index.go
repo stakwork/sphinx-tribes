4493ded703892bfc77c0cdf0609c9883402375da
kevkevinpal <oapallikunnel@gmail.com>
2024-12-16 11:23:28 -0500

    feat: using negroni middleware we fixed the socket issue

--- routes/index.go
+++ routes/index.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/rs/cors"
+	"github.com/urfave/negroni"
 
 	"github.com/stakwork/sphinx-tribes/auth"
 	"github.com/stakwork/sphinx-tribes/db"
@@ -142,35 +143,25 @@ func getFromAuth(path string) (*extractResponse, error) {
 }
 
 // Middleware to handle InternalServerError
-// func internalServerErrorHandler(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		rr := &responseRecorder{ResponseWriter: w, statusCode: http.StatusOK}
-// 		next.ServeHTTP(rr, r)
+func internalServerErrorHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rr := negroni.NewResponseWriter(w)
+		next.ServeHTTP(rr, r)
 
-// 		if rr.statusCode == http.StatusInternalServerError {
-// 			fmt.Printf("Internal Server Error: %s %s\n", r.Method, r.URL.Path)
-// 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-// 		}
-// 	})
-// }
+		if rr.Status() == http.StatusInternalServerError {
+			fmt.Printf("Internal Server Error: %s %s\n", r.Method, r.URL.Path)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	})
+}
 
-// Custom ResponseWriter to capture status codes
-// type responseRecorder struct {
-// 	http.ResponseWriter
-// 	statusCode int
-// }
-
-// func (rr *responseRecorder) WriteHeader(code int) {
-// 	rr.statusCode = code
-// 	rr.ResponseWriter.WriteHeader(code)
-// }
 
 func initChi() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	// r.Use(internalServerErrorHandler)
+	r.Use(internalServerErrorHandler)
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
