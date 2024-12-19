a0bcec3522022454c51fadc7cfdaf2fa3b0b4b97
Syed Mahtab Ali <98140673+MahtabBukhari@users.noreply.github.com>
2024-12-19 18:29:02 +0500

    Create a logger util  (#2225)
    * Create a logger util
    
    * modified the logger so we dont need to instantiate first
    
    ---------
    
    Co-authored-by: kevkevinpal <oapallikunnel@gmail.com>

---
+++ utils/logger.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"log"
+	"os"
+)
+
+type Logger struct {
+	infoLogger  *log.Logger
+	errorLogger *log.Logger
+	debugLogger *log.Logger
+}
+
+var Log = Logger{
+    infoLogger:  log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile),
+    errorLogger: log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+    debugLogger: log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile),
+}
+
+func (l *Logger) Info(format string, v ...interface{}) {
+	l.infoLogger.Printf(format, v...)
+}
+
+func (l *Logger) Error(format string, v ...interface{}) {
+	l.errorLogger.Printf(format, v...)
+}
+
+func (l *Logger) Debug(format string, v ...interface{}) {
+	l.debugLogger.Printf(format, v...)
+}
