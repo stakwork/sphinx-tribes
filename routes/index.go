7a455d8cf51893a1395625212e5e90bd6a71415f
kevkevinpal <oapallikunnel@gmail.com>
2025-01-13 12:39:30 -0500

    go func

--- routes/index.go
+++ routes/index.go
@@ -2,9 +2,10 @@ package routes
 
 import (
 	"bytes"
+	"compress/gzip"
 	"context"
-	"encoding/json"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -12,7 +13,6 @@ import (
 	"runtime"
 	"strings"
 	"time"
-	"compress/gzip"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -246,57 +246,61 @@ func internalServerErrorHandler(next http.Handler) http.Handler {
 		})
 
 		defer func() {
-			posthog_key := os.Getenv("POSTHOG_KEY")
-			posthog_url := os.Getenv("POSTHOG_URL")
-			session_id := r.Header.Get("x-session-id")
-			if posthog_key != "" && posthog_url != "" && session_id != "" {
-				logger.Log.Info("Sending to Posthog")
-				client, _ := posthog.NewWithConfig(
-					posthog_key,
-					posthog.Config{
-						Endpoint: posthog_url,
-					},
-				)
-				defer client.Close()
-				hexCompressed, _ := compressToHex(elements_chain)
-				_ = client.Enqueue(posthog.Capture{
-					DistinctId: session_id,         // Unique ID for the user
-					Event:      "backend_api_call", // The event name
-					Properties: map[string]interface{}{
-						"$session_id":     session_id,
-						"$event_type":     "backend_api_call",
-						"$elements_chain": hexCompressed,
-					},
-				})
-			}
+			go func() {
+				posthog_key := os.Getenv("POSTHOG_KEY")
+				posthog_url := os.Getenv("POSTHOG_URL")
+				session_id := r.Header.Get("x-session-id")
+				if posthog_key != "" && posthog_url != "" && session_id != "" {
+					logger.Log.Info("Sending to Posthog")
+					client, _ := posthog.NewWithConfig(
+						posthog_key,
+						posthog.Config{
+							Endpoint: posthog_url,
+						},
+					)
+					defer client.Close()
+					hexCompressed, _ := compressToHex(elements_chain)
+					_ = client.Enqueue(posthog.Capture{
+						DistinctId: session_id,         // Unique ID for the user
+						Event:      "backend_api_call", // The event name
+						Properties: map[string]interface{}{
+							"$session_id":     session_id,
+							"$event_type":     "backend_api_call",
+							"$elements_chain": hexCompressed,
+						},
+					})
+				}
+			}()
 		}()
 
 		defer func() {
-			if err := recover(); err != nil {
-				// Get stack trace
-				buf := make([]byte, 4096)
-				n := runtime.Stack(buf, true)
-				stackTrace := string(buf[:n])
+			go func() {
+				if err := recover(); err != nil {
+					// Get stack trace
+					buf := make([]byte, 4096)
+					n := runtime.Stack(buf, true)
+					stackTrace := string(buf[:n])
 
-				// Format stack trace to edge list
-				edgeList := utils.FormatStacktraceToEdgeList(stackTrace, err)
+					// Format stack trace to edge list
+					edgeList := utils.FormatStacktraceToEdgeList(stackTrace, err)
 
-				logger.Log.Error("Internal Server Error: %s %s\nError: %v\nStack Trace:\n%s\nEdge List:\n%+v\n",
-					r.Method,
-					r.URL.Path,
-					err,
-					stackTrace,
-					utils.PrettyPrintEdgeList(edgeList),
-				)
+					logger.Log.Error("Internal Server Error: %s %s\nError: %v\nStack Trace:\n%s\nEdge List:\n%+v\n",
+						r.Method,
+						r.URL.Path,
+						err,
+						stackTrace,
+						utils.PrettyPrintEdgeList(edgeList),
+					)
 
-				go func() {
-					if err := sendEdgeListToJarvis(edgeList); err != nil {
-						logger.Log.Error("Error sending to Jarvis: %v\n", err)
-					}
-				}()
+					go func() {
+						if err := sendEdgeListToJarvis(edgeList); err != nil {
+							logger.Log.Error("Error sending to Jarvis: %v\n", err)
+						}
+					}()
 
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-			}
+					http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+			}()
 		}()
 
 		next.ServeHTTP(rr, r)
