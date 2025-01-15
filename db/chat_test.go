86f84708b83ae0b9f1dea009e1a6eab959f5edfb
AhsanFarooqDev <ahsanfarooqdevtech@gmail.com>
2025-01-15 12:09:38 +0500

    unit test fixed

--- db/chat_test.go
+++ db/chat_test.go
@@ -558,7 +558,7 @@ func TestGetAllChatsForWorkspace(t *testing.T) {
 		expectError bool
 	}{
 		{
-			name:        "Valid Workspace ID with Chat",
+			name:        "Valid Workspace ID with Chats",
 			workspaceID: "validWorkspaceWithChats",
 			setup: func(workspaceID string) {
 				TestDB.db.Create(&Chat{ID: "1", WorkspaceID: workspaceID, UpdatedAt: currentTime})
