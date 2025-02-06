325d1d10633da850a0132578557e9c8842292bf8
MahtabBukhari <mahtabali50447@gmail.com>
2025-02-06 18:19:15 +0500

    correct related unit test

--- handlers/features.go
+++ handlers/features.go
@@ -599,6 +599,7 @@ func (oh *featureHandler) GetFeatureStories(w http.ResponseWriter, r *http.Reque
 	if err := websocket.WebsocketPool.SendTicketMessage(ticketMsg); err != nil {
 		log.Printf("Failed to send websocket message: %v", err)
 		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode("Failed to send websocket message")
 		return
 	}
 
