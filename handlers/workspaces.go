40a592a0ae2abf672224fb2c138f0174a3dca661
elraphty <elraphtyofficial@gmail.com>
2025-02-14 17:59:56 +0100

    added checks for multiple entry for workspace

--- handlers/workspaces.go
+++ handlers/workspaces.go
@@ -1139,6 +1139,12 @@ func GetAllUserWorkspaces(pubkey string) []db.Workspace {
 		workspace := db.DB.GetWorkspaceByUuid(uuid)
 		bountyCount := db.DB.GetWorkspaceBountyCount(uuid)
 		hasRole := db.UserHasAccess(pubkey, uuid, db.ViewReport)
+
+		// don't add workspace to the list if user is the owner of the workspace
+		if workspace.OwnerPubKey == pubkey {
+			continue
+		}
+
 		// don't add deleted workspaces to the list
 		if !workspace.Deleted {
 			if hasRole {
@@ -1149,7 +1155,18 @@ func GetAllUserWorkspaces(pubkey string) []db.Workspace {
 			}
 			workspace.BountyCount = bountyCount
 
-			workspaces = append(workspaces, workspace)
+			// check if workspace has already been added to the list
+			alreadyAdded := false
+			for _, existingWorkspace := range workspaces {
+				if existingWorkspace.Uuid == workspace.Uuid {
+					alreadyAdded = true
+					continue
+				}
+			}
+
+			if !alreadyAdded {
+				workspaces = append(workspaces, workspace)
+			}
 		}
 	}
 
