396b3f16f466d427d0eeb3dbf51782d5309b48f8
Evan Feenstra <evanfeenstra@mac.lan>
2025-02-27 13:00:43 -0800

    another try

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,5 +4,3 @@ git clone https://github.com/stakwork/sphinx-tribes-frontend
 
 cd sphinx-tribes
 
-echo "DATABASE_URL=postgres://test_user:test_password@localhost:5432/test_db" > .env
-echo "LN_JWT_KEY=notasecretstring" >> .env
