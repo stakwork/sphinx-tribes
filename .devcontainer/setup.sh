28bf913c794a33f7d39bf4c3f59c710e59947943
Evanfeenstra <evanfeenstra@gmail.com>
2025-03-05 21:28:33 -0800

    gh cli, auto open ports

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -18,3 +18,7 @@ echo "Inserting dummy data...."
 
 psql $DB -f docker/dummy-data/people.sql
 psql $DB -f docker/dummy-data/paid-bounties.sql
+
+gh codespace ports visibility 5002:public -c $CODESPACE_NAME
+gh codespace ports visibility 13008:public -c $CODESPACE_NAME
+gh codespace ports visibility 15552:public -c $CODESPACE_NAME
