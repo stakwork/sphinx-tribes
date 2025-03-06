952bb79ae7a7ecf9ba7444db821ddf4ee695ab0d
Evanfeenstra <evanfeenstra@gmail.com>
2025-03-05 20:43:51 -0800

    devcontainer sql wait

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,4 +2,19 @@
 
 cd /workspaces
 
-git clone https://github.com/stakwork/sphinx-tribes-frontend 
+git clone https://github.com/stakwork/sphinx-tribes-frontend
+
+cd sphinx-tribes
+
+DB=postgres://postgres:postgres@localhost:5432/postgres
+
+until psql $DB -c '\q'
+do
+  echo "Waiting for PostgreSQL to become available..."
+  sleep 5
+done
+
+echo "Inserting dummy data...."
+
+psql $DB -f docker/dummy-data/people.sql
+psql $DB -f docker/dummy-data/paid-bounties.sql
