06bceeda01e8a37b7bf9617c46d633d323a07583
Evanfeenstra <evanfeenstra@gmail.com>
2025-03-06 10:10:29 -0800

    more setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,9 +8,27 @@ cd sphinx-tribes
 
 DB=postgres://postgres:postgres@localhost:5432/postgres
 
+# Wait for backend to be ready
+until [ "$(curl -s -m 1 http://localhost:15552/health 2>/dev/null)" = "{\"ok\":true}" ]
+do
+  echo "Waiting for staklink to become ready..."
+  sleep 5
+done
+
+# start the services defined in staklink options
+curl -s -m 1 http://localhost:15552/start
+
+# Wait for PostgreSQL to become available
 until psql $DB -c '\q'
 do
   echo "Waiting for PostgreSQL to become available..."
+  sleep 5
+done
+
+# Wait for backend to be ready
+until [ "$(curl -s -m 1 http://localhost:5002/tribes 2>/dev/null)" = "[]" ]
+do
+  echo "Waiting for backend to become ready..."
   sleep 5
 done
 
